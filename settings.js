296f87ed11b47c9f044bc132d25b89c391c1ffc6
Satyajit Thadeshwar <sthadeshwar@netflix.com>
2015-10-05 17:03:32 -0700

    Fixing the clone wizard for titan

--- settings.js
+++ settings.js
@@ -100,7 +100,9 @@ window.spinnakerSettings = {
       defaults: {
         account: 'titantest',
         region: 'us-east-1'
-      }
+      },
+      primaryAccounts: ['test'],
+      primaryRegions: ['us-east-1'],
     }
   },
   whatsNew: {
