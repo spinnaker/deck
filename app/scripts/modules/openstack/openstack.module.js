a9ca31a5b9ad3c7d42ecddf732164028c7dd33d5
gadresushrut <gadresushrut@gmail.com>
2016-06-28 14:26:41 +0530

    (Openstack) Security Group UI Code.

--- app/scripts/modules/openstack/openstack.module.js
+++ app/scripts/modules/openstack/openstack.module.js
@@ -11,17 +11,31 @@ _.forEach(templates.keys(), function(key) {
 });
 
 module.exports = angular.module('spinnaker.openstack', [
-  require('../core/cloudProvider/cloudProvider.registry.js'),
   require('./serverGroup/configure/ServerGroupCommandBuilder.js'),
   require('./serverGroup/configure/serverGroup.configure.openstack.module.js'),
   require('./serverGroup/configure/wizard/Clone.controller.js'),
   require('./serverGroup/serverGroup.transformer.js'),
+  require('./securityGroup/configure/configure.openstack.module.js'),
+  require('./securityGroup/details/details.openstack.module.js'),
+  require('./securityGroup/transformer.js'),
+  require('./validation/applicationName.validator.js'),
+  require('./cache/configurer.service.js'),
+  require('../core/cloudProvider/cloudProvider.registry.js')
 ])
   .config(function(cloudProviderRegistryProvider) {
     cloudProviderRegistryProvider.registerProvider('openstack', {
       name: 'Openstack',
       logo: {
         path: require('./logo/openstack.logo.png')
+      },
+      securityGroup: {
+        reader: 'openstackSecurityGroupReader',
+        transformer: 'openstackSecurityGroupTransformer',
+        //TODO: Need to implement details when backend is ready.
+        //detailsTemplateUrl: require('./securityGroup/details/details.html'),
+        //detailsController: 'openstackSecurityGroupDetailsController',
+        createSecurityGroupTemplateUrl: require('./securityGroup/configure/wizard/createWizard.html'),
+        createSecurityGroupController: 'openstackUpsertSecurityGroupController'
       },
       serverGroup: {
         transformer: 'openstackServerGroupTransformer',
