c7d09e5c8c4466a2ce1c64d3fd8f61a6a379bde8
anotherchrisberry <anotherchrisberry@gmail.com>
2016-08-22 18:57:32 -0700

    (core) update to Node 6

--- webpack.config.js
+++ webpack.config.js
@@ -77,9 +77,8 @@ module.exports = {
     new CommonsChunkPlugin('init.js'),
     new HtmlWebpackPlugin({
       title: 'Spinnaker',
-      template: './app/index.html',
+      template: './app/index.ejs',
       favicon: 'app/favicon.ico',
-      inject: true,
     }),
   ],
   devServer: {
