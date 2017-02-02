09c5a0669680090dc4ece017805ad50bf0b599c6
Daniel Peach <dpeach@google.com>
2017-02-02 15:17:30 +0000

    update startup scripts

--- start.sh
+++ start.sh
@@ -1,19 +1,37 @@
 #!/bin/bash
 
+if [[ -z "$npm_package_engines_node" ]]; then
+  echo "You are probably running this script without NPM. Try running 'npm start' from the Deck directory root."
+  exit
+fi
+
 NODE_VERSION=$(node -v)
 
-if [[ $NODE_VERSION != $npm_package_engines_node ]]; then
-  if [[ -f $HOME/.nvm/nvm.sh ]]; then
-    echo "Updating your node version to $npm_package_engines_node..."
-    . $HOME/.nvm/nvm.sh
-    nvm use $npm_package_engines_node
+function update_node() {
+  echo "Updating your node version to $npm_package_engines_node..."
+  nvm use $npm_package_engines_node
+  if [[ $? != 0 ]]; then
+    echo "Installing node $npm_package_engines_node..."
+    nvm install $npm_package_engines_node
+  fi
 
-    if [[ $? != 0 ]]; then
-      echo "Installing node $npm_package_engines_node..."
-      nvm install $npm_package_engines_node
-    fi
+  if [[ ! -f $PWD/.nvmrc ]]; then
+    echo "Writing node $npm_package_engines_node to your .nvmrc..."
+    echo $npm_package_engines_node > $PWD/.nvmrc
+  elif [[ $(cat $PWD/.nvmrc) != $npm_package_engines_node ]]; then
+    echo "WARNING: Your .nvmrc specifies node $(cat $PWD/.nvmrc)."
+  fi
+}
+
+if [[ $NODE_VERSION != $npm_package_engines_node ]]; then
+  if [[ -f /etc/profile.d/nvm.sh ]]; then
+    . /etc/profile.d/nvm.sh
+    update_node
+  elif [[ -f $HOME/.nvm/nvm.sh ]]; then
+    . $HOME/.nvm/nvm.sh
+    update_node
   else
-    echo "WARNING: could not update to node $NODE_VERSION, nvm not found..."
+    echo "WARNING: could not update to node $npm_package_engines_node, nvm not found..."
   fi
 fi
 
