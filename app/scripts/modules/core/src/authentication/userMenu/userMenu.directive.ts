026f9214638d295585e2ea5fe9626ff82a72391a
Archana Sankaranarayanan <asankar@netflix.com>
2017-08-31 14:46:43 -0700

    Initial changes for making the main spinnaker header responsive

---
+++ app/scripts/modules/core/src/authentication/userMenu/userMenu.directive.ts
@@ -0,0 +1,27 @@
+import {AUTHENTICATION_INITIALIZER_SERVICE} from '../authentication.initializer.service';
+import {AUTHENTICATION_SERVICE} from '../authentication.service';
+import {SETTINGS} from 'core/config/settings';
+
+const angular = require('angular');
+
+export const AUTHENTICATION_USER_MENU = 'spinnaker.core.authentication.userMenu.directive';
+
+const ngmodule = angular.module(AUTHENTICATION_USER_MENU, [
+  AUTHENTICATION_INITIALIZER_SERVICE,
+  AUTHENTICATION_SERVICE
+]);
+
+ngmodule.directive('userMenu', function(authenticationService: any, authenticationInitializer: any) {
+  return {
+    restrict: 'E',
+    replace: true,
+    templateUrl: require('./userMenu.directive.html'),
+    link: function(scope: any) {
+      scope.authEnabled = SETTINGS.authEnabled;
+      scope.user = authenticationService.getAuthenticatedUser();
+      scope.showLogOutDropdown = () => authenticationService.getAuthenticatedUser().authenticated;
+      scope.logOut = () => authenticationInitializer.logOut();
+    }
+  };
+});
+
