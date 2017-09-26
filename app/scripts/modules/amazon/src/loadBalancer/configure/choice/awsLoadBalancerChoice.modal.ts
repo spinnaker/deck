ad7d3ae08f0bdf992121b5c1161f90d87ada775a
Paul <paul@armory.io>
2017-09-25 17:16:44 -0700

    Enable & fix existing "Create LB" stage
    INFRA_ENABLED=true env variable will properly enable the feature now.

--- app/scripts/modules/amazon/src/loadBalancer/configure/choice/awsLoadBalancerChoice.modal.ts
+++ app/scripts/modules/amazon/src/loadBalancer/configure/choice/awsLoadBalancerChoice.modal.ts
@@ -11,13 +11,23 @@ class AwsLoadBalancerChoiceCtrl implements IController {
 
   constructor(private $uibModal: any,
               private $uibModalInstance: IModalInstanceService,
-              private application: Application) {
+              private application: Application,
+              private loadBalancer: any,
+              private isNew: boolean,
+              private forPipelineConfig: boolean) {
     'ngInject';
   }
 
   public $onInit(): void {
     this.choices = LoadBalancerTypes;
     this.choice = this.choices[0];
+    if (this.loadBalancer) {
+      // If we're editing an existing LB, preset the choice based on config,
+      const needed_type = this.loadBalancer.loadBalancerType;
+      this.choice = LoadBalancerTypes.filter(function(el) {
+        return el.type === needed_type;
+      })[0];
+    }
   }
 
   public onChoiceSelection(choice: IAwsLoadBalancerConfig): void {
@@ -25,18 +35,24 @@ class AwsLoadBalancerChoiceCtrl implements IController {
   }
 
   public choose(): void {
-    this.$uibModalInstance.dismiss();
-    this.$uibModal.open({
-      templateUrl: this.choice.createTemplateUrl,
-      controller: `${this.choice.controller} as ctrl`,
-      size: 'lg',
-      resolve: {
-        application: () => this.application,
-        loadBalancer: (): null => null,
-        isNew: () => true,
-        forPipelineConfig: () => false,
-      }
-    });
+    // NOTE:  Can't just dismiss here, pipelineconfig is expecting the
+    //        result of the config to be passed back as a promise from
+    //        the initial modal, which we're closing here...
+    this.$uibModalInstance.close(
+      this.$uibModal.open({
+        templateUrl: this.choice.createTemplateUrl,
+        controller: `${this.choice.controller} as ctrl`,
+        size: 'lg',
+        resolve: {
+          application: () => this.application,
+          loadBalancer: () => this.loadBalancer,
+          isNew: () => this.isNew || (this.loadBalancer == null),
+          forPipelineConfig: () => this.forPipelineConfig,
+        }
+      }).result.then(function(newLoadBalancer: any) {
+         return newLoadBalancer;
+      })
+    );
   }
 }
 
