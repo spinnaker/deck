5a2575bd0ba860898cbe771e2fbd448c610f22aa
Archana Sankaranarayanan <asankar@netflix.com>
2017-09-27 16:07:39 -0700

    Added a static color map

--- app/scripts/modules/core/postcss.config.js
+++ app/scripts/modules/core/postcss.config.js
@@ -1,21 +1,5 @@
-const fs = require('fs');
+const colorMap = require('./src/styleguide/src/colorMap');
 
-const styleguideColors = fs.readFileSync('./app/scripts/modules/core/src/styleguide/src/styles/colors.css', 'utf-8');
-
-let colorMap = {};
-styleguideColors.split('\n').forEach(line => {
-  const colorVarRegex = /(--[A-Za-z0-9-]+)\:\s*(#[A-Za-z0-8]{6})/;
-  const colorItems = line.match(colorVarRegex);
-  if (colorItems && colorItems.length) {
-    const colorVar = colorItems[1];
-    const colorHex = colorItems[2];
-
-    if (colorVar && colorHex) {
-      colorMap[colorHex] = colorVar;
-    }
-  }
-});
-console.log('---------- Before starting postcss run --------------');
 module.exports = {
     plugins: {
         'autoprefixer': {},
--- app/scripts/modules/core/src/styleguide/src/styles/postcss.config.js
+++ app/scripts/modules/core/src/styleguide/src/styles/postcss.config.js
@@ -4,7 +4,6 @@ module.exports = {
   plugins: {
     'postcss-import': {},
     'postcss-extend': {},
-    'postcss-colorfix': {colors: {}},
     'autoprefixer': {},
     'postcss-style-guide': {
       project: 'Spinnaker',
