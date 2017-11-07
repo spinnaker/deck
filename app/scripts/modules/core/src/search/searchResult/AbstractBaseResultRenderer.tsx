07fffd4b483674cd1b8ec4a91c4c60d6e9239573
Chris Thielen <github@sandgnat.com>
2017-11-07 11:04:15 -0800

    refactor(core/search): Simplify CSS rules

--- app/scripts/modules/core/src/search/searchResult/AbstractBaseResultRenderer.tsx
+++ app/scripts/modules/core/src/search/searchResult/AbstractBaseResultRenderer.tsx
@@ -1,7 +1,7 @@
 import { get } from 'lodash';
 import * as React from 'react';
 import { BindAll } from 'lodash-decorators';
-import { capitalize } from 'lodash';
+import { capitalize, kebabCase } from 'lodash';
 
 import { NgReact } from 'core/reactShims';
 import { IResultRenderer } from './searchResultFormatter.registry';
@@ -15,6 +15,8 @@ export interface ITableColumnConfigEntry<T> {
   // cellRenderer: (item: T, key?: string, defaultValue?: string) => JSX.Element;
   cellRenderer: React.ComponentType<{ item: T, key?: string, col: ITableColumnConfigEntry<T> }>;
 }
+
+const colClass = (key: string) => `col-${kebabCase(key)}`;
 
 @BindAll()
 export abstract class AbstractBaseResultRenderer<T> implements IResultRenderer {
@@ -32,35 +34,32 @@ export abstract class AbstractBaseResultRenderer<T> implements IResultRenderer {
   }
 
   public DefaultCellRender = ({ item, col }: {item: T, col: ITableColumnConfigEntry<T>}) => (
-    <div className={`${this.getRendererClass()}-${col.key}`} key={col.key}>{get(item, col.key)}</div>
+    <div className={colClass(col.key)} key={col.key}>{get(item, col.key)}</div>
   );
 
   public HrefCellRenderer = ({ item, col }: {item: T, col: ITableColumnConfigEntry<T>}) => (
-    <div key={col.key} className={`${this.getRendererClass()}-${col.key}`} >
+    <div key={col.key} className={colClass(col.key)} >
       <a href={get(item, 'href')}>{get(item, col.key)}</a>
     </div>
   );
 
   public AccountCellRenderer = ({ item, col }: {item: T, col: ITableColumnConfigEntry<T>}) => {
-    let result: JSX.Element;
-    if (get(item, col.key)) {
-      const { AccountTag } = NgReact;
-      const accounts = get<string>(item, col.key).split(',').sort().map((account: string) => (
-        <AccountTag key={account} account={account}/>));
-      result = (
-        <div className={`${this.getRendererClass()}-account`} key="env">
-          {accounts}
-        </div>
-      );
-    } else {
-      result = (<div key="unknown" className={`${this.getRendererClass()}-account`}>-</div>);
+    const { AccountTag } = NgReact;
+    const value = get<string>(item, col.key);
+    if (!value) {
+      return <div key="unknown" className={colClass(col.key)}>-</div>;
     }
 
-    return result;
+    const accounts = value.split(',').sort();
+    return (
+      <div className={colClass(col.key)} key="env">
+        {accounts.map((account: string) => <AccountTag key={account} account={account}/>)}
+      </div>
+    );
   };
 
   public ValueOrDefaultCellRenderer = ({ item, col, defaultValue = '' }: { item: T, col: ITableColumnConfigEntry<T>, defaultValue: string }) => (
-    <div className={`${this.getRendererClass()}-${col.key}`} key={col.key}>{get(item, col.key) || defaultValue}</div>
+    <div className={`search-${col.key}`} key={col.key}>{get(item, col.key) || defaultValue}</div>
   );
 
   public scrollToTop(): void {
@@ -73,9 +72,9 @@ export abstract class AbstractBaseResultRenderer<T> implements IResultRenderer {
     }
   }
 
-  private HeaderCell = ({ column }: { column: ITableColumnConfigEntry<any> }) => (
-    <div className={`${this.getRendererClass()}-header ${this.getRendererClass()}-${column.key}`}>
-      {column.label || capitalize(column.key)}
+  private HeaderCell = ({ col }: { col: ITableColumnConfigEntry<any> }) => (
+    <div className={colClass(col.key)}>
+      {col.label || capitalize(col.key)}
     </div>
   );
 
@@ -85,13 +84,13 @@ export abstract class AbstractBaseResultRenderer<T> implements IResultRenderer {
 
     return (
       <div className="table-header">
-        {columnConfig.map(column => <HeaderCell key={column.key} column={column} /> )}
+        {columnConfig.map(column => <HeaderCell key={column.key} col={column} /> )}
       </div>
     );
   };
 
   private DataRow = ({ item }: { item: T }) => {
-    const rowClass = `${this.getRendererClass()}-row small`;
+    const rowClass = `table-row small`;
 
     return (
       <div key={this.getKey(item)} className={rowClass}>
@@ -107,7 +106,7 @@ export abstract class AbstractBaseResultRenderer<T> implements IResultRenderer {
     const sortedItems = this.sortItems(items || []);
 
     return (
-      <div ref={this.refCallback} className={`table ${this.getRendererClass()}-table`}>
+      <div ref={this.refCallback} className={`table table-search-results table-search-results-${this.getRendererClass()}`}>
         <HeaderRow/>
         <div className="table-contents flex-fill">
           {sortedItems.map(item => <DataRow key={this.getKey(item)} item={item}/>)}
