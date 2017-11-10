2d551f68ac5d8214244f2b832bc7012b4d3dc1c5
Daniel Peach <dpeach@google.com>
2017-11-10 15:40:28 -0500

    feat(core): server group manager data source

--- app/scripts/modules/core/src/serverGroupManager/serverGroupManager.module.ts
+++ app/scripts/modules/core/src/serverGroupManager/serverGroupManager.module.ts
@@ -1,8 +1,10 @@
 import { module } from 'angular';
 
+import { SERVER_GROUP_MANAGER_DATA_SOURCE } from './serverGroupManager.dataSource';
 import { SERVER_GROUP_MANAGER_SERVICE } from './serverGroupManager.service';
 
 export const SERVER_GROUP_MANAGER_MODULE = 'spinnaker.core.serverGroupManager.module';
 module(SERVER_GROUP_MANAGER_MODULE, [
+  SERVER_GROUP_MANAGER_DATA_SOURCE,
   SERVER_GROUP_MANAGER_SERVICE,
 ]);
