29348047083bd5b74767c853a5d457659c57f15a
Erik Munson <erik@ipsumcreative.com>
2018-04-19 13:52:21 -0700

    Add a handy comment about strings

--- app/scripts/modules/core/src/utils/unicodeBase64.ts
+++ app/scripts/modules/core/src/utils/unicodeBase64.ts
@@ -1,3 +1,5 @@
+// The native atob() doesn't know how to decode all unicode chars because Strings are 16-bit.
+// This escapes the unicode chars after atob() tries to process them, and re-decodes them.
 export const decodeUnicodeBase64 = (encodedString: string) =>
   decodeURIComponent(
     atob(encodedString)
