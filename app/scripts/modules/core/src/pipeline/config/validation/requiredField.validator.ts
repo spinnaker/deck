73e71200a025ef466547e934abf4559909b47a9a
Eric Zimanyi <ezimanyi@google.com>
2018-05-11 18:20:19 -0400

    Factor required field validation logic into smaller functions

--- app/scripts/modules/core/src/pipeline/config/validation/requiredField.validator.ts
+++ app/scripts/modules/core/src/pipeline/config/validation/requiredField.validator.ts
@@ -9,11 +9,16 @@ import {
   PipelineConfigValidator,
 } from './pipelineConfig.validator';
 
-export interface IRequiredFieldValidationConfig extends IValidatorConfig {
+export interface IRequiredField {
   fieldName: string;
   fieldLabel?: string;
+}
+
+export interface IBaseRequiredFieldValidationConfig extends IValidatorConfig {
   message?: string;
 }
+
+export type IRequiredFieldValidationConfig = IRequiredField & IBaseRequiredFieldValidationConfig;
 
 export class RequiredFieldValidator implements IStageOrTriggerValidator {
   public validate(
@@ -22,24 +27,42 @@ export class RequiredFieldValidator implements IStageOrTriggerValidator {
     validationConfig: IRequiredFieldValidationConfig,
     config: IStageOrTriggerTypeConfig,
   ): string {
-    if (
-      pipeline.strategy === true &&
-      ['cluster', 'regions', 'zones', 'credentials'].includes(validationConfig.fieldName)
-    ) {
-      return null;
-    }
-
-    let fieldLabel: string = validationConfig.fieldLabel || validationConfig.fieldName;
-    fieldLabel = fieldLabel.charAt(0).toUpperCase() + fieldLabel.substr(1);
-    const validationMessage =
-      validationConfig.message || `<strong>${fieldLabel}</strong> is a required field for ${config.label} stages.`;
-    const fieldExists = has(stage, validationConfig.fieldName);
-    const field: any = get(stage, validationConfig.fieldName);
-
-    if (!fieldExists || (!field && field !== 0) || (field instanceof Array && field.length === 0)) {
-      return validationMessage;
+    if (!this.passesValidation(pipeline, stage, validationConfig)) {
+      return this.validationMessage(validationConfig, config);
     }
     return null;
+  }
+
+  protected passesValidation(
+    pipeline: IPipeline,
+    stage: IStage | ITrigger,
+    validationConfig: IRequiredFieldValidationConfig,
+  ): boolean {
+    return this.fieldIsValid(pipeline, stage, validationConfig.fieldName);
+  }
+
+  protected validationMessage(
+    validationConfig: IRequiredFieldValidationConfig,
+    config: IStageOrTriggerTypeConfig,
+  ): string {
+    let fieldLabel: string = this.printableFieldLabel(validationConfig);
+    return validationConfig.message || `<strong>${fieldLabel}</strong> is a required field for ${config.label} stages.`;
+  }
+
+  protected printableFieldLabel(field: IRequiredField): string {
+    let fieldLabel: string = field.fieldLabel || field.fieldName;
+    return fieldLabel.charAt(0).toUpperCase() + fieldLabel.substr(1);
+  }
+
+  protected fieldIsValid(pipeline: IPipeline, stage: IStage | ITrigger, fieldName: string): boolean {
+    if (pipeline.strategy === true && ['cluster', 'regions', 'zones', 'credentials'].includes(fieldName)) {
+      return true;
+    }
+
+    const fieldExists = has(stage, fieldName);
+    const field: any = get(stage, fieldName);
+
+    return fieldExists && (field || field === 0) && !(field instanceof Array && field.length === 0);
   }
 }
 
