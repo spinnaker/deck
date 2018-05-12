47715f9cead746c7c31dbfa93cb328fbe13dc862
Eric Zimanyi <ezimanyi@google.com>
2018-05-12 00:07:07 -0400

    Add module to validate at least one field in list present

---
+++ app/scripts/modules/core/src/pipeline/config/validation/anyFieldRequired.validator.ts
@@ -0,0 +1,47 @@
+import { module } from 'angular';
+import { map, some } from 'lodash';
+
+import { IPipeline, IStage, IStageOrTriggerTypeConfig, ITrigger } from 'core/domain';
+import { BaseRequiredFieldValidator, IRequiredField } from './baseRequiredField.validator';
+import { PIPELINE_CONFIG_VALIDATOR, PipelineConfigValidator } from './pipelineConfig.validator';
+import { IBaseRequiredFieldValidationConfig } from 'core/pipeline/config/validation/baseRequiredField.validator';
+
+export interface IMultiRequiredField extends IBaseRequiredFieldValidationConfig {
+  fields: IRequiredField[];
+}
+
+export type IAnyFieldRequiredValidationConfig = IBaseRequiredFieldValidationConfig & IMultiRequiredField;
+
+export class AnyFieldRequiredValidator extends BaseRequiredFieldValidator {
+  protected passesValidation(
+    pipeline: IPipeline,
+    stage: IStage | ITrigger,
+    validationConfig: IAnyFieldRequiredValidationConfig,
+  ): boolean {
+    return some(validationConfig.fields, (requiredField: IRequiredField) => {
+      return this.fieldIsValid(pipeline, stage, requiredField.fieldName);
+    });
+  }
+
+  protected validationMessage(
+    validationConfig: IAnyFieldRequiredValidationConfig,
+    config: IStageOrTriggerTypeConfig,
+  ): string {
+    let fieldString: string = map(validationConfig.fields, (requiredField: IRequiredField) =>
+      this.printableFieldLabel(requiredField),
+    ).join(', ');
+    return (
+      validationConfig.message ||
+      `At least one of the following fields must be supplied for ${
+        config.label
+      } stages: <strong>${fieldString}</strong>.`
+    );
+  }
+}
+
+export const ANY_FIELD_REQUIRED_VALIDATOR = 'spinnaker.core.pipeline.config.validation.anyFieldRequired';
+module(ANY_FIELD_REQUIRED_VALIDATOR, [PIPELINE_CONFIG_VALIDATOR])
+  .service('anyFieldRequiredValidator', AnyFieldRequiredValidator)
+  .run((pipelineConfigValidator: PipelineConfigValidator, anyFieldRequiredValidator: AnyFieldRequiredValidator) => {
+    pipelineConfigValidator.registerValidator('anyFieldRequired', anyFieldRequiredValidator);
+  });
