788f58575d18548766ba6a2b069bfa15b94560df
Alan Quach <aquach@netflix.com>
2018-09-11 14:40:12 -0700

    refactor(core): All region-select-field to use RegionSelectField.tsx

--- app/scripts/modules/core/src/region/regionSelectField.directive.js
+++ app/scripts/modules/core/src/region/regionSelectField.directive.js
@@ -1,13 +1,72 @@
 'use strict';
 
 const angular = require('angular');
+import { react2angular } from 'react2angular';
+
+import { RegionSelectField } from './RegionSelectField';
 
 module.exports = angular
   .module('spinnaker.core.region.regionSelectField.directive', [])
+  .component(
+    'reactRegionSelectField',
+    react2angular(RegionSelectField, [
+      'regions',
+      'component',
+      'field',
+      'account',
+      'provider',
+      'onChange',
+      'labelColumns',
+      'fieldColumns',
+      'readOnly',
+    ]),
+  )
+  .component('regionSelectFieldProxy', {
+    controllerAs: 'vm',
+    template: `
+      <react-region-select-field regions="vm.regions"
+                                 component="vm.component"
+                                 field="vm.field"
+                                 account="vm.account"
+                                 provider="vm.provider"
+                                 on-change="vm.propagate"
+                                 label-columns="vm.labelColumns"
+                                 field-columns="vm.fieldColumns"
+                                 read-only="vm.readOnly"></react-region-select-field>
+    `,
+    bindings: {
+      regions: '=',
+      component: '=',
+      field: '<',
+      account: '=',
+      provider: '=',
+      onChange: '&',
+      labelColumns: '<',
+      fieldColumns: '<',
+      readOnly: '=',
+    },
+    controller: function() {
+      var vm = this;
+      vm.propagate = function(data) {
+        vm.component[vm.field] = data;
+        vm.onChange();
+      };
+    },
+  })
   .directive('regionSelectField', function() {
     return {
       restrict: 'E',
-      templateUrl: require('./regionSelectField.directive.html'),
+      template: `
+        <region-select-field-proxy regions="regions"
+                                   component="component"
+                                   field="field"
+                                   account="account"
+                                   provider="provider"
+                                   on-change="onChange()"
+                                   label-columns="labelColumns"
+                                   field-columns="fieldColumns"
+                                   read-only="readOnly"></region-select-field-proxy>
+      `,
       scope: {
         regions: '=',
         component: '=',
