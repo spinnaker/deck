c2f6f0fa3075d9cb0ab4b07425c094db8d663c9e
Alan Quach <aquach@netflix.com>
2018-12-07 11:17:40 -0800

    feat(runJob): reordered imports

--- app/scripts/modules/core/src/pipeline/config/stages/preconfiguredJob/preconfiguredJobStage.module.ts
+++ app/scripts/modules/core/src/pipeline/config/stages/preconfiguredJob/preconfiguredJobStage.module.ts
@@ -1,8 +1,8 @@
 import { module } from 'angular';
 
-import { PRECONFIGUREDJOB_STAGE } from './preconfiguredJobStage';
-import { STAGE_CORE_MODULE } from '../core/stage.core.module';
 import { TIME_FORMATTERS } from 'core/utils/timeFormatters';
+import { STAGE_CORE_MODULE } from '../core/stage.core.module';
+import { PRECONFIGUREDJOB_STAGE } from './preconfiguredJobStage';
 
 export const PRECONFIGUREDJOB_STAGE_MODULE = 'spinnaker.core.pipeline.stage.preconfiguredjob';
 module(PRECONFIGUREDJOB_STAGE_MODULE, [
