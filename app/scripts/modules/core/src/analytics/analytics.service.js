53ef12b00e97d8ca53f6aa219f4ceeb192079e73
Matt Duftler <duftler@google.com>
2018-12-13 10:04:24 -0500

    fix(core): Cherry-pick "do not load GA script if not configured (#6177)". (#6196)

--- app/scripts/modules/core/src/analytics/analytics.service.js
+++ app/scripts/modules/core/src/analytics/analytics.service.js
@@ -7,7 +7,13 @@ const angular = require('angular');
 module.exports = angular
   .module('spinnaker.core.analytics.service', [require('angulartics'), require('angulartics-google-analytics')])
   .run(function($window) {
-    if (SETTINGS.analytics.ga) {
-      $window.ga('create', SETTINGS.analytics.ga, 'auto');
+    if (SETTINGS.analytics.ga || SETTINGS.analytics.customConfig) {
+      $window.addEventListener('load', () => {
+        if (SETTINGS.analytics.customConfig) {
+          $window.ga('create', SETTINGS.analytics.ga, SETTINGS.analytics.customConfig);
+        } else {
+          $window.ga('create', SETTINGS.analytics.ga, 'auto');
+        }
+      });
     }
   });
