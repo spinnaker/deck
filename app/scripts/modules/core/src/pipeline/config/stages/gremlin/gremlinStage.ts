4018a523b1a99172ac1d1b5f2ce6f958dda3149a
Matt Jacobs <mattrjacobs@gmail.com>
2019-01-09 10:37:13 -0800

    feat(gremlin): Gremlin UI which takes API key input then fetches command/target templates for further selection

---
+++ app/scripts/modules/core/src/pipeline/config/stages/gremlin/gremlinStage.ts
@@ -0,0 +1,19 @@
+import { Registry } from 'core/registry';
+
+import { ExecutionDetailsTasks } from '../core';
+import { GremlinExecutionDetails } from './GremlinExecutionDetails';
+import { GremlinStageConfig } from './GremlinStageConfig';
+
+Registry.pipeline.registerStage({
+  label: 'Gremlin',
+  description: 'Runs a chaos experiment using Gremlin',
+  key: 'gremlin',
+  component: GremlinStageConfig,
+  executionDetailsSections: [GremlinExecutionDetails, ExecutionDetailsTasks],
+  strategy: true,
+  validators: [
+    { type: 'requiredField', fieldName: 'gremlinCommandTemplateId' },
+    { type: 'requiredField', fieldName: 'gremlinTargetTemplateId' },
+    { type: 'requiredField', fieldName: 'gremlinApiKey' },
+  ],
+});
