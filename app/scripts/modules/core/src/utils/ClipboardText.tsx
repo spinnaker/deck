65b662c4b1f17b190af0d01d796de7c75cd20ec8
DarrenN <info@v25media.com>
2019-01-22 18:20:29 -0500

    Updates ClipboardText to more closely mimic existing Angular component

--- app/scripts/modules/core/src/utils/ClipboardText.tsx
+++ app/scripts/modules/core/src/utils/ClipboardText.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+
+import './ClipboardText.less';
 
 export interface IClipboardTextProps {
   text: string;
@@ -8,39 +11,78 @@ export interface IClipboardTextProps {
  * Places text in an invisible input field so we can auto-focus and select the text
  * then copy it to the clipboard onClick. Used in labels found in components like
  * ManifestStatus to make it easier to grab data from the UI.
+ *
+ * This component mimics utils/clipboard/copyToClipboard.component.ts but
+ * since the text is placed in an invisible input its very easy to select
+ * if the copy fails.
  */
 export class ClipboardText extends React.Component<IClipboardTextProps> {
+  public state = {
+    tooltipCopy: false,
+  };
+
   private textRef: React.RefObject<HTMLInputElement> = React.createRef();
 
   private inputStyle = {
     borderWidth: '0px',
+    display: 'inline-block',
     backgroundColor: 'transparent',
   };
 
+  /**
+   * Focuses on the input element and attempts to copy to the clipboard.
+   * Also updates state.tooltipCopy with a success/fail message, which is
+   * reset after 3s. The selection is immediately blur'd so you shouldn't
+   * see much of it during the copy.
+   */
   public handleClick = (e: React.SyntheticEvent) => {
     e.preventDefault();
+    const { text } = this.props;
     const node: HTMLInputElement = this.textRef.current;
     node.focus();
     node.select();
 
     try {
       document.execCommand('copy');
+      node.blur();
+      this.setState({ tooltipCopy: `Copied ${text}` });
+      window.setTimeout(this.resetToolTip, 3000);
     } catch (e) {
-      /* don't do anything */
+      this.setState({ tooltipCopy: "Couldn't copy!" });
     }
+  };
+
+  public resetToolTip = () => {
+    this.setState({ tooltipCopy: false });
   };
 
   public render() {
     const { text } = this.props;
+    const { tooltipCopy } = this.state;
+
+    const copy = tooltipCopy || `Copy ${text}`;
+    const tooltip = <Tooltip id={`clipboardText-${text.replace(' ', '-')}`}>{copy}</Tooltip>;
+
     return (
-      <input
-        onClick={this.handleClick}
-        onChange={e => e} // no-op to prevent warnings
-        ref={this.textRef}
-        value={text}
-        type="text"
-        style={this.inputStyle}
-      />
+      <React.Fragment>
+        <input
+          onChange={e => e} // no-op to prevent warnings
+          ref={this.textRef}
+          value={text}
+          type="text"
+          style={this.inputStyle}
+        />
+        <OverlayTrigger placement="top" overlay={tooltip}>
+          <button
+            onClick={this.handleClick}
+            className="btn btn-xs btn-default clipboard-btn"
+            uib-tooltip={`Copy ${text}`}
+            aria-label="Copy to clipboard"
+          >
+            <span className="glyphicon glyphicon-copy" />
+          </button>
+        </OverlayTrigger>
+      </React.Fragment>
     );
   }
 }
