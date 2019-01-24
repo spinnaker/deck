51cacd620b1b3ccf8183e10b11d3da2c4183e6d2
Zach Schnackel <info@zslabs.com>
2019-01-24 15:09:10 -0500

    Fetch data from API

--- app/scripts/modules/core/src/pipeline/config/stages/gremlin/GremlinStageConfig.tsx
+++ app/scripts/modules/core/src/pipeline/config/stages/gremlin/GremlinStageConfig.tsx
@@ -1,14 +1,83 @@
 import * as React from 'react';
 
+import { API } from '@spinnaker/core';
 import { IStageConfigProps, StageConfigField } from 'core/pipeline';
+import { Observable } from 'rxjs';
+import Select from 'react-select';
 
 export class GremlinStageConfig extends React.Component<IStageConfigProps> {
+  public ENDPOINT_COMMANDS = `${API.baseUrl}/gremlin/templates/command`;
+  public ENDPOINT_TARGETS = `${API.baseUrl}/gremlin/templates/target`;
+
+  public state = { commands: [], targets: [] };
+
+  public componentDidMount() {
+    this.checkInitialLoad();
+  }
+
+  private checkInitialLoad = () => {
+    const { stage } = this.props;
+
+    if (stage.gremlinApiKey) {
+      this.fetchAPIData();
+    }
+  };
+
+  private fetchCommands = apiKey => {
+    return Observable.fromPromise(
+      API.one('gremlin/templates/command')
+        .post({
+          apiKey,
+        })
+        .then(
+          results => {
+            return results;
+          },
+          () => {
+            return [];
+          },
+        ),
+    );
+  };
+
+  private fetchTargets = apiKey => {
+    return Observable.fromPromise(
+      API.one('gremlin/templates/target')
+        .post({
+          apiKey,
+        })
+        .then(
+          results => {
+            return results;
+          },
+          () => {
+            return [];
+          },
+        ),
+    );
+  };
+
+  private fetchAPIData = () => {
+    const {
+      stage: { gremlinApiKey },
+    } = this.props;
+
+    // Get the data from all the necessary sources before rendering
+    Observable.forkJoin(this.fetchCommands(gremlinApiKey), this.fetchTargets(gremlinApiKey)).subscribe(results => {
+      this.setState({
+        commands: results[0],
+        targets: results[1],
+      });
+    });
+  };
+
   private onChange = (name: string, value: string) => {
     this.props.updateStageField({ [name]: value });
   };
 
   public render() {
     const { stage } = this.props;
+    console.log(this.state);
 
     return (
       <div className="form-horizontal">
@@ -17,25 +86,28 @@ export class GremlinStageConfig extends React.Component<IStageConfigProps> {
             name="gremlinApiKey"
             className="form-control input"
             type="text"
-            value={stage.gremlinApiKey}
+            value={stage.gremlinApiKey || ''}
             onChange={e => this.onChange(e.target.name, e.target.value)}
           />
+          <button onClick={this.fetchAPIData} type="button" className="btn btn-sm btn-default">
+            Fetch
+          </button>
         </StageConfigField>
-        <StageConfigField label="Command Template ID">
+        <StageConfigField label="Command Template">
           <input
             name="gremlinCommandTemplateId"
             className="form-control input"
             type="text"
-            value={stage.gremlinCommandTemplateId}
+            value={stage.gremlinCommandTemplateId || ''}
             onChange={e => this.onChange(e.target.name, e.target.value)}
           />
         </StageConfigField>
-        <StageConfigField label="Target Template ID">
+        <StageConfigField label="Target Template">
           <input
             name="gremlinTargetTemplateId"
             className="form-control input"
             type="text"
-            value={stage.gremlinTargetTemplateId}
+            value={stage.gremlinTargetTemplateId || ''}
             onChange={e => this.onChange(e.target.name, e.target.value)}
           />
         </StageConfigField>
