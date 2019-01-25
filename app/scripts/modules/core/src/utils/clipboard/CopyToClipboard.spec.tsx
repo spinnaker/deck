85557d6b39f307d5428d5a481f504634e8d694f3
DarrenN <info@v25media.com>
2019-01-25 10:52:21 -0500

    feat(kubernetes/v2): adds test for tooltip overlay values

--- app/scripts/modules/core/src/utils/clipboard/CopyToClipboard.spec.tsx
+++ app/scripts/modules/core/src/utils/clipboard/CopyToClipboard.spec.tsx
@@ -9,4 +9,18 @@ describe('<CopyToClipboard />', () => {
     const input = wrapper.find('input');
     expect(input.get(0).props.value).toEqual('Rebel Girl');
   });
+
+  it('Mouseover/click triggers overlay with toolTip', () => {
+    const wrapper = mount(<CopyToClipboard toolTip="Copy Rebel Girl" value="Rebel Girl" />);
+    const button = wrapper.find('button');
+    button.simulate('mouseOver');
+
+    // Grab the overlay from document by generated ID
+    const overlay = document.getElementById('clipboardValue-Rebel-Girl');
+    expect(overlay.innerText).toEqual('Copy Rebel Girl');
+
+    // Click replaces overlay text with padding + Copied!
+    button.simulate('click');
+    expect(overlay.innerText).toEqual('    Copied!    ');
+  });
 });
