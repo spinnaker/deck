1893996a3fb89fa2d98656d9640e62f6f18889fd
Clay McCoy <github@claymccoy.com>
2019-03-08 11:32:59 -0600

    Remove angular from React save pipelines stage

--- app/scripts/modules/core/src/pipeline/config/stages/savePipelines/SavePipelinesStageConfig.tsx
+++ app/scripts/modules/core/src/pipeline/config/stages/savePipelines/SavePipelinesStageConfig.tsx
@@ -3,28 +3,26 @@ import * as React from 'react';
 import { IStageConfigProps, StageConfigField } from 'core/pipeline';
 import { IArtifact, IExpectedArtifact, StageArtifactSelector } from '@spinnaker/core';
 
-export class SavePipelinesStageConfig extends React.Component<IStageConfigProps> {
-  public render() {
-    const { stage, pipeline } = this.props;
-    return (
-      <div className="container-fluid form-horizontal">
-        <StageConfigField label="Expected Artifact" fieldColumns={8}>
-          <StageArtifactSelector
-            pipeline={pipeline}
-            stage={stage}
-            expectedArtifactId={this.props.stage.pipelinesArtifactId}
-            artifact={this.props.stage.pipelinesArtifact}
-            onArtifactEdited={(artifact: IArtifact) => {
-              this.props.updateStageField({ pipelinesArtifact: artifact });
-              this.props.updateStageField({ pipelinesArtifactId: null });
-            }}
-            onExpectedArtifactSelected={(expectedArtifact: IExpectedArtifact) => {
-              this.props.updateStageField({ pipelinesArtifactId: expectedArtifact.id });
-              this.props.updateStageField({ pipelinesArtifact: null });
-            }}
-          />
-        </StageConfigField>
-      </div>
-    );
-  }
-}
+export const SavePipelinesStageConfig: React.SFC<IStageConfigProps> = props => {
+  const { stage, pipeline } = props;
+  return (
+    <div className="container-fluid form-horizontal">
+      <StageConfigField label="Expected Artifact" fieldColumns={8}>
+        <StageArtifactSelector
+          pipeline={pipeline}
+          stage={stage}
+          expectedArtifactId={props.stage.pipelinesArtifactId}
+          artifact={props.stage.pipelinesArtifact}
+          onArtifactEdited={(artifact: IArtifact) => {
+            props.updateStageField({ pipelinesArtifact: artifact });
+            props.updateStageField({ pipelinesArtifactId: null });
+          }}
+          onExpectedArtifactSelected={(expectedArtifact: IExpectedArtifact) => {
+            props.updateStageField({ pipelinesArtifactId: expectedArtifact.id });
+            props.updateStageField({ pipelinesArtifact: null });
+          }}
+        />
+      </StageConfigField>
+    </div>
+  );
+};
--- app/scripts/modules/core/src/pipeline/config/stages/savePipelines/savePipelinesStage.ts
+++ app/scripts/modules/core/src/pipeline/config/stages/savePipelines/savePipelinesStage.ts
@@ -6,21 +6,14 @@ import {
   Registry,
 } from '@spinnaker/core';
 import { SavePipelinesStageConfig } from 'core/pipeline/config/stages/savePipelines/SavePipelinesStageConfig';
-import { module } from 'angular';
 
-export const SAVE_PIPELINES_STAGE = 'spinnaker.core.pipeline.stage.savePipelinesStage';
-
-module(SAVE_PIPELINES_STAGE, []).config(() => {
-  Registry.pipeline.registerStage({
-    label: 'Save Pipelines',
-    description: 'Saves pipelines defined in an artifact.',
-    key: 'savePipelinesFromArtifact',
-    component: SavePipelinesStageConfig,
-    executionDetailsSections: [ExecutionDetailsTasks, ExecutionArtifactTab],
-    producesArtifacts: false,
-    defaultTimeoutMs: 30 * 60 * 1000, // 30 minutes
-    validators: [],
-    artifactExtractor: ExpectedArtifactService.accumulateArtifacts(['pipelinesArtifactId', 'requiredArtifactIds']),
-    artifactRemover: ArtifactReferenceService.removeArtifactFromFields(['pipelinesArtifactId', 'requiredArtifactIds']),
-  });
+Registry.pipeline.registerStage({
+  label: 'Save Pipelines',
+  description: 'Saves pipelines defined in an artifact.',
+  key: 'savePipelinesFromArtifact',
+  component: SavePipelinesStageConfig,
+  executionDetailsSections: [ExecutionDetailsTasks, ExecutionArtifactTab],
+  defaultTimeoutMs: 30 * 60 * 1000, // 30 minutes
+  artifactExtractor: ExpectedArtifactService.accumulateArtifacts(['pipelinesArtifactId', 'requiredArtifactIds']),
+  artifactRemover: ArtifactReferenceService.removeArtifactFromFields(['pipelinesArtifactId', 'requiredArtifactIds']),
 });
