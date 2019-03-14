6572bc5031778c873ef4f034a17ea9659e87265c
Clay McCoy <github@claymccoy.com>
2019-03-14 17:28:09 -0500

    Add summary of pipeline save results

--- app/scripts/modules/core/src/pipeline/config/stages/savePipelines/savePipelinesStage.ts
+++ app/scripts/modules/core/src/pipeline/config/stages/savePipelines/savePipelinesStage.ts
@@ -6,13 +6,14 @@ import {
   Registry,
 } from '@spinnaker/core';
 import { SavePipelinesStageConfig } from 'core/pipeline/config/stages/savePipelines/SavePipelinesStageConfig';
+import { SavePipelinesResultsTab } from 'core/pipeline/config/stages/savePipelines/SavePipelinesResultsTab';
 
 Registry.pipeline.registerStage({
   label: 'Save Pipelines',
   description: 'Saves pipelines defined in an artifact.',
   key: 'savePipelinesFromArtifact',
   component: SavePipelinesStageConfig,
-  executionDetailsSections: [ExecutionDetailsTasks, ExecutionArtifactTab],
+  executionDetailsSections: [ExecutionDetailsTasks, ExecutionArtifactTab, SavePipelinesResultsTab],
   defaultTimeoutMs: 30 * 60 * 1000, // 30 minutes
   artifactExtractor: ExpectedArtifactService.accumulateArtifacts(['pipelinesArtifactId', 'requiredArtifactIds']),
   artifactRemover: ArtifactReferenceService.removeArtifactFromFields(['pipelinesArtifactId', 'requiredArtifactIds']),
