f804ae41fed74622614e60b5bfdf6d9a1834de2b
Jammy Louie <jlouie@pivotal.io>
2019-03-15 15:16:30 -0400

    chore(core): upgrade the version to formik 1.4.1

--- app/scripts/modules/cloudfoundry/src/serverGroup/configure/wizard/sections/configurationSettings/ConfigurationSettings.cf.tsx
+++ app/scripts/modules/cloudfoundry/src/serverGroup/configure/wizard/sections/configurationSettings/ConfigurationSettings.cf.tsx
@@ -148,7 +148,7 @@ export class CloudFoundryServerGroupConfigurationSettings
                 <CloudFoundryRadioButtonInput
                   value={direct ? 'direct' : 'artifact'}
                   options={[{ label: 'Artifact', value: 'artifact' }, { label: 'Form', value: 'direct' }]}
-                  onChange={e => this.manifestSourceUpdated(e.target.value)}
+                  onChange={(e: any) => this.manifestSourceUpdated(e.target.value)}
                 />
               </div>
             </div>
