c757b521cb72f7c6f06a541d1ab961ba622f5816
chrisb <anotherchrisberry@gmail.com>
2019-04-23 21:34:27 -0700

    chore(core): remove hipchat notification module

--- settings.js
+++ settings.js
@@ -109,10 +109,6 @@ window.spinnakerSettings = {
     googlechat: {
       enabled: true,
     },
-    hipchat: {
-      botName: 'Skynet T-800',
-      enabled: true,
-    },
     pubsub: {
       enabled: true,
     },
