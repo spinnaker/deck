6029b883618e5d8e958b12a3ef89cea3a020d308
Erik Munson <erik@ipsumcreative.com>
2019-06-27 21:39:40 -0700

    module typo
    Co-Authored-By: Chris Berry <chrisb@netflix.com>

--- app/scripts/modules/core/src/managed/managedResourceDetailsIndicator.component.ts
+++ app/scripts/modules/core/src/managed/managedResourceDetailsIndicator.component.ts
@@ -3,7 +3,7 @@ import { react2angular } from 'react2angular';
 
 import { ManagedResourceDetailsIndicator } from './ManagedResourceDetailsIndicator';
 
-export const MANAGED_RESOURCE_DETAILS_INDICATOR = 'spinnaker.amazon.managed.resourceDetailsIndicator.component';
+export const MANAGED_RESOURCE_DETAILS_INDICATOR = 'spinnaker.core.managed.resourceDetailsIndicator.component';
 module(MANAGED_RESOURCE_DETAILS_INDICATOR, []).component(
   'managedResourceDetailsIndicator',
   react2angular(ManagedResourceDetailsIndicator, ['entityTags']),
