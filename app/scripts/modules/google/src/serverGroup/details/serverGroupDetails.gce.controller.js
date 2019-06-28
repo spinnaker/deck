f709c09c9f9d14b9ec4d2d03f26af88633eec8e0
maggieneterval <mneterval@google.com>
2019-06-28 11:09:47 -0400

    feat(google): instance template disk UI adjustments

--- app/scripts/modules/google/src/serverGroup/details/serverGroupDetails.gce.controller.js
+++ app/scripts/modules/google/src/serverGroup/details/serverGroupDetails.gce.controller.js
@@ -171,59 +171,17 @@ module.exports = angular
 
       const prepareDiskDescriptions = () => {
         if (_.has(this.serverGroup, 'launchConfig.instanceTemplate.properties.disks')) {
-          const diskDescriptions = [];
-
-          this.serverGroup.launchConfig.instanceTemplate.properties.disks.forEach(disk => {
-            const diskLabel = disk.initializeParams.diskType + ':' + disk.initializeParams.diskSizeGb;
-            const existingDiskDescription = _.find(diskDescriptions, description => {
-              return description.bareLabel === diskLabel;
-            });
-
-            if (existingDiskDescription) {
-              existingDiskDescription.count++;
-              existingDiskDescription.countSuffix = ' (×' + existingDiskDescription.count + ')';
-              existingDiskDescription.sourceImages = getSourceImage(disk)
-                ? [getSourceImage(disk)].concat(existingDiskDescription.sourceImages)
-                : existingDiskDescription.sourceImages;
-            } else {
-              diskDescriptions.push({
-                bareLabel: diskLabel,
-                count: 1,
-                countSuffix: '',
-                finalLabel:
-                  translateDiskType(disk.initializeParams.diskType) + ': ' + disk.initializeParams.diskSizeGb + 'GB',
-                sourceImages: getSourceImage(disk) ? [getSourceImage(disk)] : [],
-              });
-            }
-          });
-
-          diskDescriptions.forEach(description => {
-            if (!description.sourceImages.length) {
-              return;
-            }
-
-            description.sourceImages = _.uniq(description.sourceImages);
-
-            switch (description.count) {
-              case 0:
-                break;
-              case 1:
-                if (description.sourceImages[0]) {
-                  description.helpField = `This disk uses the source image <em>${description.sourceImages[0]}</em>.`;
-                }
-                break;
-              default:
-                description.helpField = `
-                These disks use the following source images:
-                <ul>
-                  ${description.sourceImages.map(image => `<li><em>${image}</em></li>`).join('')}
-                </ul>
-              `;
-                break;
-            }
-          });
-
-          this.serverGroup.diskDescriptions = diskDescriptions;
+          this.serverGroup.diskDescriptions = this.serverGroup.launchConfig.instanceTemplate.properties.disks.map(
+            disk => {
+              return {
+                diskLabel: disk.boot ? 'Boot Disk' : 'Disk',
+                typeLabel: `${translateDiskType(disk.initializeParams.diskType)}: ${
+                  disk.initializeParams.diskSizeGb
+                }GB`,
+                imageLabel: `Image: ${getSourceImage(disk)}`,
+              };
+            },
+          );
         }
       };
 
