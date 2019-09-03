8b874def1f88f5bcf43bf139094173f0eb232226
Jammy Louie <jlouie@pivotal.io>
2019-09-03 11:08:35 -0400

    update the notifications field to use formik

--- app/scripts/modules/core/src/notification/selector/types/githubstatus/GithubNotificationType.tsx
+++ app/scripts/modules/core/src/notification/selector/types/githubstatus/GithubNotificationType.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-import { FormikFormField, TextInput, Validators } from 'core/presentation';
+
+import { FormikFormField, TextInput } from 'core/presentation';
 import { INotificationTypeCustomConfig } from 'core/domain';
 
 export class GithubNotificationType extends React.Component<INotificationTypeCustomConfig> {
@@ -7,30 +8,17 @@ export class GithubNotificationType extends React.Component<INotificationTypeCus
     const { fieldName } = this.props;
     return (
       <>
-        <div className="sp-margin-m-bottom">
-          <div className={'form-group'}>
-            <label className={'col-md-4 sm-label-right'}>Github Repository</label>
-            <div className="col-md-6">
-              <FormikFormField
-                name={fieldName ? `${fieldName}.repo` : 'repo'}
-                input={props => <TextInput inputClassName={'form-control input-sm'} {...props} />}
-                required={true}
-              />
-            </div>
-          </div>
-        </div>
-        <div className="sp-margin-m-bottom">
-          <div className={'form-group'}>
-            <label className={'col-md-4 sm-label-right'}>Commit SHA</label>
-            <div className="col-md-6">
-              <FormikFormField
-                name={fieldName ? `${fieldName}.commit` : 'commit'}
-                validate={Validators.emailValue('Please enter a valid email address')}
-                input={props => <TextInput inputClassName={'form-control input-sm'} {...props} />}
-              />
-            </div>
-          </div>
-        </div>
+        <FormikFormField
+          label="Github Repository"
+          name={fieldName ? `${fieldName}.repo` : 'repo'}
+          input={props => <TextInput inputClassName={'form-control input-sm'} {...props} />}
+          required={true}
+        />
+        <FormikFormField
+          label="Commit SHA"
+          name={fieldName ? `${fieldName}.commit` : 'commit'}
+          input={props => <TextInput inputClassName={'form-control input-sm'} {...props} />}
+        />
       </>
     );
   }
