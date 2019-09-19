481ae581d4488f131036f5c3cde239f1b32267b7
Erik Munson <emunson@netflix.com>
2019-09-19 14:12:03 -0700

    Provide custom layout to entire manual execution modal, convert some vanilla markup

--- app/scripts/modules/core/src/pipeline/manualExecution/DryRun.tsx
+++ app/scripts/modules/core/src/pipeline/manualExecution/DryRun.tsx
@@ -3,16 +3,11 @@ import { CheckboxInput, FormikFormField } from 'core/presentation';
 import { HelpField } from 'core/help';
 
 export const DryRun = () => (
-  <div className="form-group">
-    <label className="col-md-4 sm-label-right">
-      Dry run <HelpField id="execution.dryRun" />
-    </label>
-    <div className="col-md-6">
-      <FormikFormField
-        name="dryRun"
-        fastField={false}
-        input={props => <CheckboxInput {...props} text="Run a test execution" />}
-      />
-    </div>
-  </div>
+  <FormikFormField
+    name="dryRun"
+    label="Dry run"
+    fastField={false}
+    help={<HelpField id="execution.dryRun" />}
+    input={props => <CheckboxInput {...props} text="Run a test execution" />}
+  />
 );
