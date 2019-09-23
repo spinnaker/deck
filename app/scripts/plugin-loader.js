9d8350783319c1366966c613d0bf898278e6957c
Brandon Powell <brandon.powell@armory.io>
2019-09-23 11:41:46 -0500

    chore(plugins): added comments on plugin-loader

--- app/scripts/plugin-loader.js
+++ app/scripts/plugin-loader.js
@@ -1,5 +1,7 @@
 import { Registry } from '@spinnaker/core';
 
+// This method appends the plugin script location to the bottom of
+// the page to load the plugin
 function loadPluginScript(plugin) {
   return new Promise((resolve, reject) => {
     var scriptTag = document.createElement('script');
@@ -11,6 +13,11 @@ function loadPluginScript(plugin) {
   });
 }
 
+// This method grabs all plugins that are defined in Spinnaker settings
+// and then loops through all of them. It then calls the function above to
+// appends a script tag to the page from where the plugin location and then
+// initializes the plugin by passing in the registry for plugins to register
+// themselves as stages
 export function initPlugins() {
   const plugins = window.spinnakerSettings.plugins;
   window.spinnakerSettings.onPluginLoaded = plugin => plugin.initialize(Registry);
