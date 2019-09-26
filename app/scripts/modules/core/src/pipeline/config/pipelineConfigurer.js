spinnaker/deck

commit 10ae1cd17dd38e4852978ba16c04427d7cc3ec1c

fix(core/pipeline): Fix revert button regression for templated pipelines (#7427)

app/scripts/modules/core/src/pipeline/config/pipelineConfigurer.js
@@ -437,7 +437,6 @@ module.exports = angular
         $scope.$applyAsync(() => {
           const original = getOriginal();
           $scope.pipeline = _.clone(original);
-          $scope.renderablePipeline = $scope.pipeline;
 
           if ($scope.isTemplatedPipeline) {
             const originalRenderablePipeline = getOriginalRenderablePipeline();

commit cb53eb13d4e6114167afcb33138554febbcaeed0

fix(core/pipeline): Fix revert button for non-templated pipelines
I accidentally broke this in #7427. Adding the removed line back, but only for non-templated pipelines.

app/scripts/modules/core/src/pipeline/config/pipelineConfigurer.js
@@ -440,6 +440,8 @@ module.exports = angular
                 delete $scope.renderablePipeline[key];
               }
             });
+          } else {
+            $scope.renderablePipeline = $scope.pipeline;
           }
 
           // if we were looking at a stage that no longer exists, move to the last stage