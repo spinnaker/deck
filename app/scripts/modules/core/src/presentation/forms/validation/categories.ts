e7ba60e3de4a07b8368ef50c24cf12c4de9e6dad
Chris Thielen <github@sandgnat.com>
2019-10-03 17:27:35 -0700

    test: add tests for message helper functions rename 'error' to 'validationMessage' in some spots

--- app/scripts/modules/core/src/presentation/forms/validation/categories.ts
+++ app/scripts/modules/core/src/presentation/forms/validation/categories.ts
@@ -36,16 +36,16 @@ export const warningMessage = buildCategoryMessage('warning');
 // A regular expression which captures the category label and validation message from a validation message
 // I.e., for the string: "Error: There was a fatal error"
 // this captures "Error" and "There was a fatal error"
-const errorMessageRegexp = new RegExp(`^(${labels.join('|')}): (.*)$`);
+const validationMessageRegexp = new RegExp(`^(${labels.join('|')}): (.*)$`);
 
 // Takes an errorMessage with embedded category and extracts the category and message
 // Example:  "Error: there was an error" => ['error', 'there was an error']
 // Example:  "this message has no explicit category" => ['error', 'this message has no explicit category']
-export const categorizeErrorMessage = (errorMessage: string): [IValidationCategory, string] => {
-  const result = errorMessageRegexp.exec(errorMessage);
+export const categorizeValidationMessage = (validationMessage: string): [IValidationCategory, string] => {
+  const result = validationMessageRegexp.exec(validationMessage);
   if (!result) {
     // If no known category label was found embedded in the error message, default the category to 'error'
-    return ['error', errorMessage];
+    return ['error', validationMessage];
   }
   const [label, message] = result.slice(1);
   const status = inverseLabels[label];
@@ -54,13 +54,13 @@ export const categorizeErrorMessage = (errorMessage: string): [IValidationCatego
 };
 
 /** Organizes errors from an errors object into ICategorizedErrors buckets. */
-export const categorizeErrors = (errors: any): ICategorizedErrors => {
+export const categorizeValidationMessages = (errors: any): ICategorizedErrors => {
   // Build an empty Categorized Errors object
   const categories: ICategorizedErrors = statusKeys.reduce((acc, status) => ({ ...acc, [status]: {} }), {}) as any;
 
   // Given a path and a validation message, store the validation message into the same path of the correct category
   const storeMessageInCategory = (path: string, errorMessage: any) => {
-    const [status, message] = categorizeErrorMessage(errorMessage);
+    const [status, message] = categorizeValidationMessage(errorMessage);
 
     if (message) {
       set(categories[status], path, message);
