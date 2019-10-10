0d04a8120f243ac7a16abcb67f9be9596f2e7ed3
Chris Thielen <github@sandgnat.com>
2019-10-10 14:56:44 -0700

    fix(core/presentation): Make all DOMPurify'd links open in a new window

--- app/scripts/modules/core/src/presentation/presentation.module.js
+++ app/scripts/modules/core/src/presentation/presentation.module.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import * as DOMPurify from 'dompurify';
+
 const angular = require('angular');
 
 import { AUTO_SCROLL_DIRECTIVE } from 'core/presentation/autoScroll/autoScroll.directive';
@@ -14,15 +16,33 @@ import './details.less';
 import './main.less';
 import './navPopover.less';
 
-module.exports = angular.module('spinnaker.core.presentation', [
-  ANY_FIELD_FILTER,
-  AUTO_SCROLL_DIRECTIVE,
-  PAGE_NAVIGATOR_COMPONENT,
-  PAGE_SECTION_COMPONENT,
-  require('./collapsibleSection/collapsibleSection.directive').name,
-  require('./isVisible/isVisible.directive').name,
-  ROBOT_TO_HUMAN_FILTER,
-  require('./sortToggle/sorttoggle.directive').name,
-  require('./percent.filter').name,
-  REPLACE_FILTER,
-]);
+module.exports = angular
+  .module('spinnaker.core.presentation', [
+    ANY_FIELD_FILTER,
+    AUTO_SCROLL_DIRECTIVE,
+    PAGE_NAVIGATOR_COMPONENT,
+    PAGE_SECTION_COMPONENT,
+    require('./collapsibleSection/collapsibleSection.directive').name,
+    require('./isVisible/isVisible.directive').name,
+    ROBOT_TO_HUMAN_FILTER,
+    require('./sortToggle/sorttoggle.directive').name,
+    require('./percent.filter').name,
+    REPLACE_FILTER,
+  ])
+  .run(() => {
+    // Add a hook to make all DOMPurify'd links open a new window
+    // See: https://github.com/cure53/DOMPurify/tree/master/demos#hook-to-open-all-links-in-a-new-window-link
+    DOMPurify.addHook('afterSanitizeAttributes', function(node) {
+      // set all elements owning target to target=_blank
+      if ('target' in node) {
+        node.setAttribute('target', '_blank');
+        // prevent https://www.owasp.org/index.php/Reverse_Tabnabbing
+        node.setAttribute('rel', 'noopener noreferrer');
+      }
+      // set non-HTML/MathML links to xlink:show=new
+      if (!node.hasAttribute('target') && (node.hasAttribute('xlink:href') || node.hasAttribute('href'))) {
+        node.setAttribute('xlink:show', 'new');
+      }
+      return node;
+    });
+  });
