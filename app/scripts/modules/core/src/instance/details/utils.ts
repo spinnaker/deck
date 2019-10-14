a0eb66488a1231434ccd83e074c754300704625c
chebebrand <chebebrand@netflix.com>
2019-10-14 12:01:18 -0700

    Add types to util functions

--- app/scripts/modules/core/src/instance/details/utils.ts
+++ app/scripts/modules/core/src/instance/details/utils.ts
@@ -1,15 +1,26 @@
 import { flatten, keyBy } from 'lodash';
+import {
+  IAmazonApplicationLoadBalancer,
+  IAmazonNetworkLoadBalancer,
+  ITargetGroup,
+  IAmazonHealth,
+} from '@spinnaker/amazon';
 
-export const getAllTargetGroups = loadBalancers => {
+export const getAllTargetGroups = (
+  loadBalancers: IAmazonApplicationLoadBalancer[] | IAmazonNetworkLoadBalancer[],
+): { [name: string]: ITargetGroup } => {
   const allTargetGroups = loadBalancers.map(d => d.targetGroups);
   const targetGroups = keyBy(flatten(allTargetGroups), 'name');
   return targetGroups;
 };
 
-export const applyHealthCheckInfoToTargetGroups = (healthMetrics, targetGroups) => {
+export const applyHealthCheckInfoToTargetGroups = (
+  healthMetrics: IAmazonHealth[],
+  targetGroups: { [name: string]: ITargetGroup },
+) => {
   healthMetrics.forEach(metric => {
     if (metric.type === 'TargetGroup') {
-      metric.targetGroups.forEach(tg => {
+      metric.targetGroups.forEach((tg: ITargetGroup) => {
         const group = targetGroups[tg.name];
         tg.healthCheckProtocol = group.healthCheckProtocol.toLowerCase();
         tg.healthCheckPath = `:${group.healthCheckPort}${group.healthCheckPath}`;
