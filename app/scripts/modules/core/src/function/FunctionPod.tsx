0d0473483a4b097cbb5f0a02ce0ece76fb07097f
Shalaka Sidmul <sidmuls@amazon.com>
2019-10-16 15:57:49 -0700

    refactor(provider/aws): Converting Function class component into function component.
    This is a refactoring change as requested by reviewer @caseyhebebrand
    
    Author: @sidmuls

--- app/scripts/modules/core/src/function/FunctionPod.tsx
+++ app/scripts/modules/core/src/function/FunctionPod.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import { AccountTag } from 'core/account';
 import { Application } from 'core/application/application.model';
 import { IFunctionGroup } from 'core/domain';
-import { Function } from './Function';
+import Function from './Function';
 
 import './functionPod.less';
 
