0dbd5f28b08447ef3925a0becfcde419399a4c5f
Shalaka Sidmul <sidmuls@amazon.com>
2019-10-16 09:45:03 -0700

    feat(provider/aws): Function listing and searching functionality
    This change adds a new tab for functions and facilitates listing existing functions and searching functions listed.
    Corresponding provider specific changes are included in this change for AWS.
    
    Authors: @sidmuls and @nabebe

---
+++ app/scripts/modules/core/src/domain/IFunction.ts
@@ -0,0 +1,29 @@
+import { ITaggedEntity } from './ITaggedEntity';
+
+export interface IFunctionSourceData {
+  cloudProvider?: string;
+  name?: string;
+  provider?: string;
+  type?: string;
+}
+
+export interface IFunction extends ITaggedEntity {
+  account?: string;
+  cloudProvider?: string;
+  description?: string;
+  name?: string;
+  functionName?: string;
+  provider?: string;
+  region?: string;
+  searchField?: string;
+  type?: string;
+  vpcId?: string;
+  vpcName?: string;
+}
+
+export interface IFunctionGroup {
+  heading: string;
+  functionDef?: IFunction;
+  subgroups?: IFunctionGroup[];
+  searchField?: string;
+}
