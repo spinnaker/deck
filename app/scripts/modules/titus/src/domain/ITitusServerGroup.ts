3dd32045935fa2a7814421e355d01829504ffd3a
Chris Thielen <github@sandgnat.com>
2019-10-29 14:03:58 -0700

    feat(titus/domain): extract ITitusResources interface and add a few fields to ITitusServerGroup

--- app/scripts/modules/titus/src/domain/ITitusServerGroup.ts
+++ app/scripts/modules/titus/src/domain/ITitusServerGroup.ts
@@ -4,12 +4,23 @@ import { IJobDisruptionBudget } from './IJobDisruptionBudget';
 import { ITitusPolicy } from './ITitusScalingPolicy';
 import { ITitusServiceJobProcesses } from './ITitusServiceJobProcesses';
 
+export interface ITitusResources {
+  cpu: number;
+  disk: number;
+  gpu: number;
+  memory: number;
+  networkMbps: number;
+}
+
 export interface ITitusServerGroup extends IServerGroup {
   capacityGroup?: string;
   disruptionBudget?: IJobDisruptionBudget;
+  entryPoint: string;
+  iamProfile: string;
   id?: string;
   image?: ITitusImage;
   migrationPolicy?: { type: string };
+  resources: ITitusResources;
   scalingPolicies?: ITitusPolicy[];
   serviceJobProcesses?: ITitusServiceJobProcesses;
   targetGroups?: string[];
