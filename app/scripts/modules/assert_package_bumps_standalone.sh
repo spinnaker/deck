107f1048bb55717af58af92fae7d02579948ab73
Alan Quach <aquach@netflix.com>
2019-11-13 12:25:03 -0800

    test(npm): Using TRAVIS_BRANCH

--- app/scripts/modules/assert_package_bumps_standalone.sh
+++ app/scripts/modules/assert_package_bumps_standalone.sh
@@ -6,20 +6,24 @@ PKGJSONCHANGED="Version change detected in package.json"
 ONLYVERSIONCHANGED="Version change must be the only line changed in package.json"
 ONLYPKGJSONCHANGED="package.json (in apps/scripts/modules) must be the only files changed in a pull request with version bumps"
 
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+TARGET_BRANCH=${TRAVIS_BRANCH:-master}
+echo "TARGET_BRANCH=$TARGET_BRANCH"
+
 echo "Merging from target branch to get minimal diff."
-git merge upstream/master
+git merge $TARGET_BRANCH
 
 for PKGJSON in `ls app/scripts/modules/*/package.json` ; do
   MODULE=`basename $(dirname $PKGJSON)`
   echo "==================================================="
   echo "Checking $MODULE"
   echo "==================================================="
-  HAS_PKG_BUMP=`git diff upstream/master -- $PKGJSON | grep '"version"' | wc -l`
+  HAS_PKG_BUMP=`git diff $TARGET_BRANCH -- $PKGJSON | grep '"version"' | wc -l`
   if [ $HAS_PKG_BUMP -ne 0 ] ; then
     echo " [ YES  ] $PKGJSONCHANGED"
 
     # Ensuring that the version change is the only change in package.json
-    PKG_JSON_OTHER_CHANGES=`git diff --numstat upstream/master -- $PKGJSON | cut -f 1`
+    PKG_JSON_OTHER_CHANGES=`git diff --numstat $TARGET_BRANCH -- $PKGJSON | cut -f 1`
     if [ $PKG_JSON_OTHER_CHANGES -ne 1 ] ; then
       echo " [ FAIL ] $ONLYVERSIONCHANGED"
       echo ""
@@ -27,7 +31,7 @@ for PKGJSON in `ls app/scripts/modules/*/package.json` ; do
       echo "Failure details (git diff of package.json)"
       echo "=========================================="
       echo ""
-      git diff upstream/master -- $PKGJSON
+      git diff $TARGET_BRANCH -- $PKGJSON
       echo ""
       echo "=========================================="
       exit 2
@@ -36,7 +40,7 @@ for PKGJSON in `ls app/scripts/modules/*/package.json` ; do
     fi
 
     # checking that the only files changed are apps/scripts/modules/*/package.json
-    OTHER_FILES_CHANGED=`git diff --name-only upstream/master | grep -v "apps/scripts/modules/*/package.json" | wc -l`
+    OTHER_FILES_CHANGED=`git diff --name-only $TARGET_BRANCH | grep -v "apps/scripts/modules/*/package.json" | wc -l`
     if [ $OTHER_FILES_CHANGED -ne 0 ] ; then
       echo " [ FAIL ] $ONLYPKGJSONCHANGED"
       echo ""
@@ -44,7 +48,7 @@ for PKGJSON in `ls app/scripts/modules/*/package.json` ; do
       echo "Failure details (list of all files changed)"
       echo "==========================================="
       echo ""
-      git diff --name-only upstream/master
+      git diff --name-only $TARGET_BRANCH
       echo ""
       echo "==========================================="
       exit 1
