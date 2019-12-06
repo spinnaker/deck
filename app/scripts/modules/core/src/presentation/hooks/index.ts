be68f9ea1dd14c1811e8ccfd3dfa24634744c0f5
maggieneterval <mneterval@google.com>
2019-12-06 14:56:25 -0500

    Revert "chore(core/presentation): expose some new hooks to consumers"
    This reverts commit da8047c9790b5478c36f13b22ab7e9f959101a1d.

--- app/scripts/modules/core/src/presentation/hooks/index.ts
+++ app/scripts/modules/core/src/presentation/hooks/index.ts
@@ -1,7 +1,4 @@
-export * from './useContainerClassNames.hook';
 export * from './useData.hook';
 export * from './useDebouncedValue.hook';
-export * from './useEventListener.hook';
 export * from './useIsMountedRef.hook';
-export * from './useLatestCallback.hook';
 export * from './useLatestPromise.hook';
