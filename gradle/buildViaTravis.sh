6773c40d3a55cc758230be88573543bf6c05bfe4
maggieneterval <mneterval@google.com>
2019-12-06 13:33:12 -0500

    test(ci): test if karma fails with gradle build

--- gradle/buildViaTravis.sh
+++ gradle/buildViaTravis.sh
@@ -2,12 +2,8 @@
 # This script will build the project.
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo -e "Skipping build (running unit tests only) for Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
-  source ~/.nvm/nvm.sh
-  NODE_JS_VERSION=`node -e 'console.log(require("./package.json").engines.node.replace(/[^\d\.]/g, ""))'`;
-  nvm use $NODE_JS_VERSION
-
-  ./node_modules/.bin/karma start --single-run
+  echo -e "Build Pull Request #$TRAVIS_PULL_REQUEST => Branch [$TRAVIS_BRANCH]"
+  ./gradlew -Prelease.useLastTag=true build
 
 elif [ "$TRAVIS_PULL_REQUEST" == "false" ] && [ "$TRAVIS_TAG" == "" ]; then
   echo -e 'Build Branch with Snapshot => Branch ['$TRAVIS_BRANCH']'
