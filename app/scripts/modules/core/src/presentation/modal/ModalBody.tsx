91cea1e07337db1a1258d45dde7c03f0a0672d7c
maggieneterval <mneterval@google.com>
2019-12-06 14:55:36 -0500

    Revert "feat(core/presentation): add <ModalBody/> component"
    This reverts commit 55f09bc5c5a0e489215ba8ba087349a844105dce.

--- app/scripts/modules/core/src/presentation/modal/ModalBody.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-
-import * as styles from './ModalBody.module.css';
-
-export interface IModalBodyProps {
-  children?: React.ReactNode;
-}
-
-export const ModalBody = ({ children }: IModalBodyProps) => <div className={styles.body}>{children}</div>;
