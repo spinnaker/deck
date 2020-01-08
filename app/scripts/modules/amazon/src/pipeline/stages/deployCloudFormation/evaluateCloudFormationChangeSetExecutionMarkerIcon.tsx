cb785dd7d30268cbd33c4218c600693b8d4bf28c
sergiopena <isergiopena@gmail.com>
2020-01-08 11:14:57 +0100

    Move back to class

--- app/scripts/modules/amazon/src/pipeline/stages/deployCloudFormation/evaluateCloudFormationChangeSetExecutionMarkerIcon.tsx
+++ app/scripts/modules/amazon/src/pipeline/stages/deployCloudFormation/evaluateCloudFormationChangeSetExecutionMarkerIcon.tsx
@@ -1,15 +1,20 @@
 import * as React from 'react';
-import { IExecutionMarkerIconProps } from '@spinnaker/core';
+import { IExecutionMarkerIconProps } from 'core/pipeline/config/stages/common/ExecutionMarkerIcon';
 
-export const EvaluateCloudFormationChangeSetExecutionMarkerIcon = (props: IExecutionMarkerIconProps) => {
-  const { stage } = props;
-  if (
-    stage.isRunning &&
-    stage.stages[0].context.changeSetContainsReplacement &&
-    stage.stages[0].context.actionOnReplacement === 'ask'
-  ) {
-    stage.requiresAttention = true;
-    return <span className="fa fa-child" />;
+export class EvaluateCloudFormationChangeSetExecutionMarkerIcon extends React.Component<IExecutionMarkerIconProps> {
+  constructor(props: IExecutionMarkerIconProps) {
+    super(props);
   }
-  return null;
-};
+
+  public render() {
+    if (
+      this.props.stage.isRunning &&
+      this.props.stage.stages[0].context.changeSetContainsReplacement &&
+      this.props.stage.stages[0].context.actionOnReplacement === 'ask'
+    ) {
+      this.props.stage.requiresAttention = true;
+      return <span className="fa fa-child" />;
+    }
+    return null;
+  }
+}
