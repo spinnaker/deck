52cee6834876230a94196f130492ad4caded6407
Luis Pollo <lpollo@netflix.com>
2020-01-09 14:42:07 -0800

    Address review feedback

--- app/scripts/modules/core/src/presentation/forms/inputs/TextInput.tsx
+++ app/scripts/modules/core/src/presentation/forms/inputs/TextInput.tsx
@@ -2,21 +2,29 @@ import React from 'react';
 
 import { orEmptyString, validationClassName } from './utils';
 import { IFormInputProps, OmitControlledInputPropsFrom } from './interface';
-import get = Reflect.get;
 
 export interface ITextInputProps extends IFormInputProps, OmitControlledInputPropsFrom<React.InputHTMLAttributes<any>> {
   inputClassName?: string;
+  prefix?: string;
 }
 
 export class TextInput extends React.Component<ITextInputProps> {
   public render() {
-    const { value, validation, inputClassName, ...otherProps } = this.props;
+    const { value, validation, inputClassName, prefix, ...otherProps } = this.props;
     const className = `TextInput form-control ${orEmptyString(inputClassName)} ${validationClassName(validation)}`;
-    return (
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-        <div style={{ paddingRight: '5px', fontSize: '13px' }}>{get(otherProps, 'prefix') ?? ''}</div>
-        <input className={className} type="text" autoComplete="off" value={orEmptyString(value)} {...otherProps} />
-      </div>
+    const inputTag = (
+      <input className={className} type="text" autoComplete="off" value={orEmptyString(value)} {...otherProps} />
     );
+
+    if (prefix != null) {
+      return (
+        <div className="flex-container-h middle">
+          <div className="sp-padding-xs-right body-regular">{prefix}</div>
+          {inputTag}
+        </div>
+      );
+    } else {
+      return inputTag;
+    }
   }
 }
