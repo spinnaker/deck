340349ec26c4d9b4329d263c6e7f810b961d86af
Brandon Powell <brandon.powell@armory.io>
2020-01-21 13:23:26 -0800

    fix(plugins): Fixes based on Chris T's feedback

--- app/scripts/modules/core/src/plugins/plugin.module.ts
+++ app/scripts/modules/core/src/plugins/plugin.module.ts
@@ -19,14 +19,15 @@ module(PLUGINS_MODULE, ['ui.router']).config([
       const pluginManifestLocation = '/plugin-manifest.js';
       const pluginModule = await import(/* webpackIgnore: true */ pluginManifestLocation);
 
-      if (!pluginModule || !pluginModule.PLUGINS) {
-        throw new Error(`Error loading plugins.`);
+      if (!pluginModule || !pluginModule.plugins) {
+        throw new Error(`Expected plugin-manifest.js to contain an export named 'plugins' but it did not.`);
       } else {
         // @ts-ignore
-        pluginModule.PLUGINS.forEach(plugin => pluginRegistry.register(plugin));
+        pluginModule.plugins.forEach(plugin => pluginRegistry.register(plugin));
       }
     } catch (error) {
-      console.error('No plugins found.');
+      console.error('Error registering plugin manifests');
+      console.error(error);
     }
 
     try {
--- plugin-manifest.js
+++ plugin-manifest.js
@@ -1,2 +1,2 @@
-const PLUGINS = [];
-export { PLUGINS };
+const plugins = [];
+export { plugins };
