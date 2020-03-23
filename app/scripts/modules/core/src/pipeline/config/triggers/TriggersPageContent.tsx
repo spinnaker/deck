ad367c62b56c076c8c1c1aa542e08ed7f072125f
Maggie Neterval <mneterval@google.com>
2020-03-23 14:59:03 -0400

    fix(artifacts): only remove deleted expected artifacts from stages on trigger update (#8071) (#8077)

--- app/scripts/modules/core/src/pipeline/config/triggers/TriggersPageContent.tsx
+++ app/scripts/modules/core/src/pipeline/config/triggers/TriggersPageContent.tsx
@@ -83,9 +83,12 @@ export function TriggersPageContent(props: ITriggersPageContentProps) {
       if (
         !pipelineParam.triggers.find(t => t.expectedArtifactIds && t.expectedArtifactIds.includes(expectedArtifact.id))
       ) {
-        newExpectedArtifacts.splice(findIndex(newExpectedArtifacts, e => e.id === expectedArtifact.id), 1);
+        newExpectedArtifacts.splice(
+          findIndex(newExpectedArtifacts, e => e.id === expectedArtifact.id),
+          1,
+        );
+        ArtifactReferenceService.removeReferenceFromStages(expectedArtifact.id, pipelineParam.stages);
       }
-      ArtifactReferenceService.removeReferenceFromStages(expectedArtifact.id, pipelineParam.stages);
     });
     setExpectedArtifacts(newExpectedArtifacts);
     updatePipelineConfig({ expectedArtifacts });
