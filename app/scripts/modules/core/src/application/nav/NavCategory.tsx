7880a618cc5b7ec0b6379b67602cc28193d8e100
chebebrand <chebebrand@netflix.com>
2020-04-09 13:04:18 -0700

    feat(core/nav): useDataSrouce hook and rebase

--- app/scripts/modules/core/src/application/nav/NavCategory.tsx
+++ app/scripts/modules/core/src/application/nav/NavCategory.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 import { DataSourceNotifications } from '../../entityTag/notifications/DataSourceNotifications';
-import { Icon } from '../../presentation';
+import { Icon, useDataSource, useObservable } from '../../presentation';
 
 import { ApplicationDataSource } from '../service/applicationDataSource';
-import { Application } from '../../application';
+import { Application, IFetchStatus } from '../../application';
 import { IEntityTags } from '../../domain';
 
 export interface INavCategoryProps {
@@ -14,55 +14,27 @@ export interface INavCategoryProps {
 }
 
 export const NavCategory: React.FC<INavCategoryProps> = ({ app, category, isActive }: INavCategoryProps) => {
-  const { alerts, badge, iconName, label } = category;
-  const badgeSourceData = badge ? app.getDataSource(badge).data.length : 0;
+  const { alerts, badge, iconName, key, label, status$ } = category;
+  const { data: badgeData } = useDataSource(app.getDataSource(badge || key));
+  const runningCount = badge ? badgeData.length : 0;
 
-  const [runningCount, setRunningCount] = React.useState<number>(badgeSourceData);
   const [tags, setTags] = React.useState<IEntityTags[]>(alerts || []);
-
-  const runningCountSubscription = badge ? category.refresh$.subscribe(() => setRunningCount(badgeSourceData)) : null;
-  const tagsSubscription = category.refresh$.subscribe(() => setTags(alerts || []));
-  const [runningCountSub, setRunningCountSub] = React.useState(runningCountSubscription);
-  const [tagsSub, setTagsSub] = React.useState(tagsSubscription);
-
-  const configureSubscriptions = () => {
-    setRunningCountSub(runningCountSubscription);
-    setTagsSub(tagsSub);
-  };
-
-  const clearSubscriptions = () => {
-    runningCountSub && runningCountSub.unsubscribe();
-    tagsSub.unsubscribe();
-  };
-
-  React.useEffect(() => {
-    setRunningCount(badgeSourceData);
-    setTags(alerts || []);
-    clearSubscriptions();
-    configureSubscriptions();
-    return clearSubscriptions;
-  }, [alerts, badgeSourceData]);
-
-  /**
-   *  The subscriptions coupled with useState cause some latency when setting   the initial state.
-   *  These checks help reduce the initial rendering time for tags and run counts
-   */
-
-  const showCount = (badgeSourceData && !runningCount) || runningCount;
-  const count = badgeSourceData && !runningCount ? badgeSourceData : runningCount;
-  const badgeClassNames = showCount ? 'badge-running-count' : 'badge-none';
-  const tagList = (alerts || []).length && !tags.length ? alerts : tags;
+  useObservable(status$, (status: IFetchStatus) => {
+    const newTags = status.data.map((d: ApplicationDataSource) => d.entityTags).filter((d: ApplicationDataSource) => d);
+    setTags(newTags);
+  });
+  const badgeClassNames = runningCount ? 'badge-running-count' : 'badge-none';
 
   return (
     <div className="nav-category">
-      <div className={badgeClassNames}>{showCount ? count : ''}</div>
+      <div className={badgeClassNames}>{runningCount > 0 ? runningCount : ''}</div>
       <div className="nav-item">
         {iconName && (
           <Icon className="nav-icon" name={iconName} size="extraSmall" color={isActive ? 'primary' : 'accent'} />
         )}
       </div>
       <div className="nav-item">{' ' + category.label}</div>
-      <DataSourceNotifications tags={tagList} application={app} tabName={label} />
+      <DataSourceNotifications tags={tags} application={app} tabName={label} />
     </div>
   );
 };
