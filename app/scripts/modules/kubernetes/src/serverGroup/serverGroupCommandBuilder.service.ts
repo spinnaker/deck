f9b390eeedad5b83edecbc0561ef5a0976c5f99f
Maggie Neterval <mneterval@google.com>
2020-04-30 15:28:36 -0400

    refactor(kubernetes): merge /v2 and /shared into /src

---
+++ app/scripts/modules/kubernetes/src/serverGroup/serverGroupCommandBuilder.service.ts
@@ -0,0 +1,20 @@
+import { module, IPromise } from 'angular';
+
+import { Application } from '@spinnaker/core';
+import {
+  IKubernetesManifestCommandData,
+  KubernetesManifestCommandBuilder,
+} from '../manifest/manifestCommandBuilder.service';
+
+export class KubernetesV2ServerGroupCommandBuilder {
+  public buildNewServerGroupCommand(app: Application): IPromise<IKubernetesManifestCommandData> {
+    return KubernetesManifestCommandBuilder.buildNewManifestCommand(app);
+  }
+}
+
+export const KUBERNETES_V2_SERVER_GROUP_COMMAND_BUILDER = 'spinnaker.kubernetes.v2.serverGroup.commandBuilder.service';
+
+module(KUBERNETES_V2_SERVER_GROUP_COMMAND_BUILDER, []).service(
+  'kubernetesV2ServerGroupCommandBuilder',
+  KubernetesV2ServerGroupCommandBuilder,
+);
