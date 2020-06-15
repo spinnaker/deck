1a5cbaff2338277770ed8ff5c4f66ea238c93c31
Shaunery Seale <sseale@salesforce.com>
2020-06-15 13:41:40 -0400

    turning feature back off by default

--- settings.js
+++ settings.js
@@ -74,7 +74,7 @@ window.spinnakerSettings = {
     displayTimestampsInUserLocalTime: displayTimestampsInUserLocalTime,
     dryRunEnabled: dryRunEnabled,
     entityTags: entityTagsEnabled,
-    executionMarkerInformationModal: true,
+    executionMarkerInformationModal: false,
     fiatEnabled: fiatEnabled,
     gceScaleDownControlsEnabled: gceScaleDownControlsEnabled,
     gceStatefulMigsEnabled: gceStatefulMigsEnabled,
