1e658ca694d278d4e0d803ceb4d8ff0f064a144e
chebebrand <chebebrand@netflix.com>
2020-06-22 18:50:50 -0700

    refactor(core): Convert spinnaker container to React

---
+++ app/scripts/modules/core/src/bootstrap/SpinnakerContainer.tsx
@@ -0,0 +1,34 @@
+import * as React from 'react';
+import { UIView } from '@uirouter/react';
+
+import { ReactInjector } from '../reactShims';
+
+import { CustomBanner } from '../header/customBanner/CustomBanner';
+import { SpinnakerHeader } from '../header/SpinnakerHeader';
+import { Spinner } from '../widgets/spinners/Spinner';
+
+export interface ISpinnakerContainerProps {
+  authenticating: boolean;
+  routing: boolean;
+}
+
+export const SpinnakerContainer = ({ authenticating, routing }: ISpinnakerContainerProps) => {
+  // eslint-disable-next-line
+  console.log(authenticating, routing);
+  const HeaderComponent = ReactInjector.overrideRegistry.getComponent('spinnakerHeader') || SpinnakerHeader;
+
+  return (
+    <div className="spinnaker-container grid-container">
+      {!authenticating && routing && (
+        <div className="transition-overlay">
+          <Spinner size="medium" />
+        </div>
+      )}
+      <div className="navbar-inverse grid-header">
+        <CustomBanner />
+        <HeaderComponent />
+      </div>
+      <div className="spinnaker-content grid-contents">{!authenticating && <UIView name="main" />}</div>
+    </div>
+  );
+};
