0efea571edfc97f5b7bf3c79d77c4201e1d84723
chebebrand <chebebrand@netflix.com>
2020-06-22 18:50:50 -0700

    refactor(core): Convert spinnaker container to React

--- app/scripts/modules/core/src/bootstrap/spinnaker.component.ts
+++ app/scripts/modules/core/src/bootstrap/spinnaker.component.ts
@@ -1,37 +1,21 @@
 import { IController } from 'angular';
-import { react2angular } from 'react2angular';
 
 import { bootstrapModule } from './bootstrap.module';
-import { OverrideRegistry } from 'core/overrideRegistry';
 import { IFeatures, SETTINGS } from 'core/config/settings';
-
-import { SpinnakerHeader } from 'core/header/SpinnakerHeader';
+import { IDeckRootScope } from 'core/domain';
 
 const template = `
-  <div class="spinnaker-container grid-container">
-    <div class="transition-overlay" ng-if="!authenticating && routing">
-      <loading-spinner size="'medium'"></loading-spinner>
-    </div>
-    <div class="navbar-inverse grid-header">
-      <custom-banner></custom-banner>
-      <div ng-include="$ctrl.spinnakerHeaderTemplate"></div>
-    </div>
-
-    <div class="spinnaker-content grid-contents">
-      <div ui-view="main" ng-if="!authenticating"></div>
-    </div>
-  </div>
+  <spinnaker-container authenticating="$ctrl.authenticating" routing="$ctrl.routing"></spinnaker-container>
   <notifier></notifier>
 `;
 
 class SpinnakerController implements IController {
-  public spinnakerHeaderTemplate: string;
   public feature: IFeatures;
-  public static $inject = ['overrideRegistry'];
-  constructor(overrideRegistry: OverrideRegistry) {
-    react2angular(SpinnakerHeader);
-    this.spinnakerHeaderTemplate = overrideRegistry.getTemplate('spinnakerHeader', require('./spinnakerHeader.html'));
+  public static $inject = ['$rootScope'];
+  constructor($rootScope: IDeckRootScope) {
     this.feature = SETTINGS.feature;
+    this.authenticating = $rootScope.authenticating;
+    this.routing = $rootScope.routing;
   }
 }
 
