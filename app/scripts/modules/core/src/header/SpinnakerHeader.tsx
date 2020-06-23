d6a6b4e1de0c5736381729eadc40f39994460e76
chebebrand <chebebrand@netflix.com>
2020-06-23 15:46:24 -0700

    refactor(core): Refactor SpinnakerHeader to functional component

--- app/scripts/modules/core/src/header/SpinnakerHeader.tsx
+++ app/scripts/modules/core/src/header/SpinnakerHeader.tsx
@@ -9,87 +9,82 @@ import { HelpMenu } from 'core/help/HelpMenu';
 
 import './SpinnakerHeader.css';
 
-export interface ISpinnakerHeaderState {
-  navExpanded: boolean;
-}
-
 @UIRouterContext
 @Overridable('spinnakerHeader')
-export class SpinnakerHeader extends React.Component<{}, ISpinnakerHeaderState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      navExpanded: !this.isDevicePhoneOrSmaller(),
-    };
+export class SpinnakerHeader extends React.Component<{}, {}> {
+  public render(): React.ReactElement<SpinnakerHeader> {
+    return <SpinnakerHeaderContent />;
   }
+}
 
-  public isDevicePhoneOrSmaller(): boolean {
+/**
+ * This needs to be a functional component to use an external module's hook.
+ * Currently, @Overrides only works on a class component.
+ * This is temproary until we can refactor the override component to work for functional components.
+ */
+
+export const SpinnakerHeaderContent = () => {
+  const isDevicePhoneOrSmaller = () => {
     const bodyStyles = window.getComputedStyle(document.body);
     const isPhone = bodyStyles.getPropertyValue('--is-phone');
     return isPhone.toLowerCase() === 'true';
-  }
-
-  public toggleNavItems = (): void => {
-    this.setState({
-      navExpanded: !this.state.navExpanded,
-    });
   };
+  const { UserMenu } = NgReact;
 
-  public render(): React.ReactElement<SpinnakerHeader> {
-    const { UserMenu } = NgReact;
+  const [navExpanded, setNavExpanded] = React.useState(!isDevicePhoneOrSmaller());
+  const toggleNavItems = () => setNavExpanded(!navExpanded);
 
-    return (
-      <nav className="container spinnaker-header" role="navigation" aria-label="Main Menu">
-        <div className="navbar-header horizontal middle">
-          <a className="navbar-brand flex-1" href="#">
-            SPINNAKER
-          </a>
-          <button type="button" className="navbar-toggle" onClick={this.toggleNavItems}>
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-            <span className="icon-bar" />
-          </button>
+  return (
+    <nav className="container spinnaker-header" role="navigation" aria-label="Main Menu">
+      <div className="navbar-header horizontal middle">
+        <a className="navbar-brand flex-1" href="#">
+          SPINNAKER
+        </a>
+        <button type="button" className="navbar-toggle" onClick={toggleNavItems}>
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+          <span className="icon-bar" />
+        </button>
+      </div>
+      {navExpanded && (
+        <div className="nav-container nav-items">
+          <ul className="nav nav-items flex-1 page-nav">
+            <li key="navHome">
+              <UISrefActive class="active">
+                <UISref to="home.infrastructure">
+                  <a>Search</a>
+                </UISref>
+              </UISrefActive>
+            </li>
+            <li key="navProjects">
+              <UISrefActive class="active">
+                <UISref to="home.projects">
+                  <a>Projects</a>
+                </UISref>
+              </UISrefActive>
+            </li>
+            <li key="navApplications">
+              <UISrefActive class="active">
+                <UISref to="home.applications">
+                  <a>Applications</a>
+                </UISref>
+              </UISrefActive>
+            </li>
+            <li key="navPipelineTemplates">
+              <UISrefActive class="active">
+                <UISref to="home.pipeline-templates">
+                  <a>Pipeline Templates</a>
+                </UISref>
+              </UISrefActive>
+            </li>
+          </ul>
+          <ul className="nav nav-items">
+            <UserMenu />
+            <GlobalSearch />
+            <HelpMenu />
+          </ul>
         </div>
-        {this.state.navExpanded && (
-          <div className="nav-container nav-items">
-            <ul className="nav nav-items flex-1 page-nav">
-              <li key="navHome">
-                <UISrefActive class="active">
-                  <UISref to="home.infrastructure">
-                    <a>Search</a>
-                  </UISref>
-                </UISrefActive>
-              </li>
-              <li key="navProjects">
-                <UISrefActive class="active">
-                  <UISref to="home.projects">
-                    <a>Projects</a>
-                  </UISref>
-                </UISrefActive>
-              </li>
-              <li key="navApplications">
-                <UISrefActive class="active">
-                  <UISref to="home.applications">
-                    <a>Applications</a>
-                  </UISref>
-                </UISrefActive>
-              </li>
-              <li key="navPipelineTemplates">
-                <UISrefActive class="active">
-                  <UISref to="home.pipeline-templates">
-                    <a>Pipeline Templates</a>
-                  </UISref>
-                </UISrefActive>
-              </li>
-            </ul>
-            <ul className="nav nav-items">
-              <UserMenu />
-              <GlobalSearch />
-              <HelpMenu />
-            </ul>
-          </div>
-        )}
-      </nav>
-    );
-  }
-}
+      )}
+    </nav>
+  );
+};
