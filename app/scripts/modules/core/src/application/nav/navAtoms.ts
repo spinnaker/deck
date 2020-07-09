906b9e9f6f623b4df19e5a94f04464b32349cfbe
chebebrand <chebebrand@netflix.com>
2020-07-09 13:38:15 -0700

    fix(core): Layout v1 feedback

--- app/scripts/modules/core/src/application/nav/navAtoms.ts
+++ app/scripts/modules/core/src/application/nav/navAtoms.ts
@@ -3,5 +3,7 @@ import { CollapsibleSectionStateCache } from '../../cache/collapsibleSectionStat
 
 export const verticalNavExpandedAtom = atom({
   key: 'verticalNavExpanded',
-  default: !CollapsibleSectionStateCache.isSet('verticalNav') || CollapsibleSectionStateCache.isExpanded('verticalNav'),
+  default:
+    !CollapsibleSectionStateCache.isSet('verticalNav') ||
+    (CollapsibleSectionStateCache.isSet('verticalNav') && CollapsibleSectionStateCache.isExpanded('verticalNav')),
 });
