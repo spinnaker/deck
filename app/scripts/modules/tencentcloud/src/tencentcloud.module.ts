e2725ed5a9a008fb9f24d6abca8156b4c4482aea
sean <xiaoxiaojing@coding.net>
2020-07-22 11:38:45 +0800

    fix(sercurityGroup): Create Security Group select provider tencentcloud logo 404

--- app/scripts/modules/tencentcloud/src/tencentcloud.module.ts
+++ app/scripts/modules/tencentcloud/src/tencentcloud.module.ts
@@ -1,27 +1,22 @@
 'use strict';
 
 import { module } from 'angular';
-
 import { CloudProviderRegistry, DeploymentStrategyRegistry } from '@spinnaker/core';
-import './help/tencentcloud.help';
-import { TencentcloudImageReader } from './image';
 import { TENCENTCLOUD_SEARCH_SEARCHRESULTFORMATTER } from './search/searchResultFormatter';
 import { TENCENTCLOUD_REACT_MODULE } from './reactShims/tencentcloud.react.module';
-import './validation/ApplicationNameValidator';
 import { CreateSecurityGroupModal, SecurityGroupDetail } from './securityGroup';
-
-// load all templates into the $templateCache
-const templates = require.context('./', true, /\.html$/);
-templates.keys().forEach(function(key) {
-  templates(key);
-});
+import { TencentcloudImageReader } from './image';
+import tencentcloudLogo from './logo/tencentcloud.logo.svg';
+import './validation/ApplicationNameValidator';
+import './help/tencentcloud.help';
+import './logo/tencentcloud.logo.less';
 
 export const TENCENTCLOUD_MODULE = 'spinnaker.tencentcloud';
 module(TENCENTCLOUD_MODULE, [TENCENTCLOUD_REACT_MODULE, TENCENTCLOUD_SEARCH_SEARCHRESULTFORMATTER]).config(() => {
   CloudProviderRegistry.registerProvider('tencentcloud', {
-    name: 'tencentcloud',
+    name: 'Tencentcloud',
     logo: {
-      path: require('./logo/tencentcloud.logo.svg'),
+      path: tencentcloudLogo,
     },
     image: {
       reader: TencentcloudImageReader,
