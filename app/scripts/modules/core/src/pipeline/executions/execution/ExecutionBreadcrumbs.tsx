66e9a0995b06b6b811599804c26a1497751ff875
rodrigo-espinosa <rodrigo.espinosa@salesforce.com>
2020-07-23 19:49:32 -0400

    Only show parent executions, not current one, and rename label.

--- app/scripts/modules/core/src/pipeline/executions/execution/ExecutionBreadcrumbs.tsx
+++ app/scripts/modules/core/src/pipeline/executions/execution/ExecutionBreadcrumbs.tsx
@@ -20,29 +20,9 @@ export const ExecutionBreadcrumbs: React.FC<IExecutionBreadcrumbsProps> = ({ exe
 
   return (
     <div>
-      {executions.length > 1 && <span>Execution Path: </span>}
-      {executions.length === 1 ? (
-        <UISref
-          to="home.applications.application.pipelines.executionDetails.execution"
-          params={{
-            application: executions[0].application,
-            executionId: executions[0].id,
-            executionParams: {
-              application: executions[0].application,
-              executionId: executions[0].id,
-            },
-          }}
-          options={{
-            inherit: false,
-            reload: 'home.applications.application.pipelines.executionDetails',
-          }}
-        >
-          <a style={{ display: 'inline' }} className="execution-build-number clickable">
-            {executions[0].name}
-          </a>
-        </UISref>
-      ) : (
-        executions.length > 1 &&
+      {executions.length > 1 && <span>Parent Executions: </span>}
+      {executions.length > 1 &&
+        executions.shift() &&
         [...executions].reverse().map((execution, index, array) => (
           <React.Fragment key={execution.id}>
             <UISref
@@ -71,8 +51,7 @@ export const ExecutionBreadcrumbs: React.FC<IExecutionBreadcrumbsProps> = ({ exe
               </span>
             )}
           </React.Fragment>
-        ))
-      )}
+        ))}
     </div>
   );
 };
