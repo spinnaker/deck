4736614329fe29f5dc3f1fda1b5e9bc841dd7cb8
Maggie Neterval <mneterval@google.com>
2020-07-31 12:21:48 -0400

    refactor(core): remove unused ManifestService
    For some reason, we ended up with a KubernetesManifestService and a similar but unused core ManifestService.

--- app/scripts/modules/core/src/manifest/index.ts
+++ app/scripts/modules/core/src/manifest/index.ts
@@ -2,7 +2,6 @@ export * from './ManifestWriter';
 export * from './ManifestReader';
 export * from './stage/JobStageExecutionLogs';
 export * from './IManifestSubscription';
-export * from './ManifestService';
 export * from './stage/JobManifestPodLogs';
 export * from './ManifestYaml';
 export * from './PodNameProvider';
