0e198a134da955242d155c99f593eeee3a015809
Maggie Neterval <mneterval@google.com>
2020-08-04 16:43:40 -0400

    refactor(kubernetes/pipelines): move translation from stage manifest to manifest params out of DeployStatus
    Deck currently gets the full manifests it receives from Orca in the execution context, parses some details from those full manifests to create a new request to Clouddriver's /manifests endpoint. Much of this logic was added to disambiguate CRDs of the same kind but different API group. Ideally, Deck would have no knowledge of build-in Kubernetes groups and simply receive all the information it needs to construct a valid request. We'll revisit this later, but for now, let's at least move all the logic into private methods on the KubernetesManifestService rather than having them live on the DeployStatus component. This is also important because in a future commit, we will push the logic that sets up subscriptions against the /manifest endpoint further down the tree so that each ManifestStatus component is responsible for fetching its own data.

--- app/scripts/modules/kubernetes/src/pipelines/stages/deployManifest/manifestStatus/DeployStatus.tsx
+++ app/scripts/modules/kubernetes/src/pipelines/stages/deployManifest/manifestStatus/DeployStatus.tsx
@@ -11,26 +11,6 @@ import { KubernetesManifestService, IStageManifest } from '../../../../manifest/
 
 import { ManifestStatus } from './ManifestStatus';
 
-// from https://kubernetes.io/docs/reference/generated/kubernetes-api/v1.12/
-const BUILT_IN_GROUPS = [
-  '',
-  'core',
-  'batch',
-  'apps',
-  'extensions',
-  'storage.k8s.io',
-  'apiextensions.k8s.io',
-  'apiregistration.k8s.io',
-  'policy',
-  'scheduling.k8s.io',
-  'settings.k8s.io',
-  'authorization.k8s.io',
-  'authentication.k8s.io',
-  'rbac.authorization.k8s.io',
-  'certifcates.k8s.io',
-  'networking.k8s.io',
-];
-
 export interface IManifestSubscription {
   id: string;
   unsubscribe: () => void;
@@ -76,11 +56,7 @@ export class DeployStatus extends React.Component<IExecutionDetailsSectionProps,
   }
 
   private subscribeToManifestUpdates(id: string, manifest: IStageManifest): () => void {
-    const params = {
-      account: this.props.stage.context.account,
-      name: this.scopedKind(manifest) + ' ' + manifest.metadata.name,
-      location: manifest.metadata.namespace == null ? '_' : manifest.metadata.namespace,
-    };
+    const params = KubernetesManifestService.stageManifestToManifestParams(manifest, this.props.stage.context.account);
     return KubernetesManifestService.subscribe(this.props.application, params, (updated: IManifest) => {
       const idx = this.state.subscriptions.findIndex(sub => sub.id === id);
       if (idx !== -1) {
@@ -94,26 +70,6 @@ export class DeployStatus extends React.Component<IExecutionDetailsSectionProps,
 
   private unsubscribeAll() {
     this.state.subscriptions.forEach(({ unsubscribe }) => unsubscribe());
-  }
-
-  private apiGroup(manifest: IStageManifest): string {
-    const parts = (manifest.apiVersion || '_').split('/');
-    if (parts.length < 2) {
-      return '';
-    }
-    return parts[0];
-  }
-
-  private isCRDGroup(manifest: IStageManifest): boolean {
-    return !BUILT_IN_GROUPS.includes(this.apiGroup(manifest));
-  }
-
-  private scopedKind(manifest: IStageManifest): string {
-    if (this.isCRDGroup(manifest)) {
-      return manifest.kind + '.' + this.apiGroup(manifest);
-    }
-
-    return manifest.kind;
   }
 
   private stageManifestToIManifest(manifest: IStageManifest, account: string): IManifest {
