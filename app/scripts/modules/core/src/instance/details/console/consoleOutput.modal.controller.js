b9d9833248019b3ea0a10a985cfdb2721689624b
AbdulRahman AlHamali <aalhamali@coveo.com>
2020-08-17 18:27:37 -0400

    feature(UI): Add support for displaying colors in console outputs

--- app/scripts/modules/core/src/instance/details/console/consoleOutput.modal.controller.js
+++ app/scripts/modules/core/src/instance/details/console/consoleOutput.modal.controller.js
@@ -4,6 +4,8 @@ import { InstanceReader } from '../../InstanceReader';
 
 import { module } from 'angular';
 
+import { default as AnsiUp } from 'ansi_up';
+
 export const CORE_INSTANCE_DETAILS_CONSOLE_CONSOLEOUTPUT_MODAL_CONTROLLER =
   'spinnaker.core.instance.details.console.controller';
 export const name = CORE_INSTANCE_DETAILS_CONSOLE_CONSOLEOUTPUT_MODAL_CONTROLLER; // for backwards compatibility
@@ -25,7 +27,19 @@ module(CORE_INSTANCE_DETAILS_CONSOLE_CONSOLEOUTPUT_MODAL_CONTROLLER, []).control
       $scope.vm.exception = null;
       InstanceReader.getConsoleOutput(instance.account, instance.region, instanceId, instance.provider).then(
         function(response) {
+          const ansi_up = new AnsiUp();
+
           $scope.vm.consoleOutput = response.output;
+
+          // Transform the console output to a colored HTML
+          if (Array.isArray($scope.vm.consoleOutput)) {
+            $scope.vm.consoleOutput.forEach(entry => {
+              entry.output = ansi_up.ansi_to_html(entry.output);
+            });
+          } else {
+            $scope.vm.consoleOutput = ansi_up.ansi_to_html($scope.vm.consoleOutput);
+          }
+
           $scope.vm.loading = false;
 
           if ($scope.vm.usesMultiOutput) {
