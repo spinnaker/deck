90b3e4fc578bbdbd15b4991cc69575f54811d6e8
chebebrand <chebebrand@netflix.com>
2020-10-09 15:38:24 -0700

    refactor(core): Component for instance actions dropdown

---
+++ app/scripts/modules/core/src/instance/details/InstanceActions.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+
+export interface Action {
+  label: string;
+  triggerAction: () => void;
+}
+
+type ActionGroup = Action[];
+
+export interface IInstanceActionsProps {
+  actionGroups: ActionGroup[];
+}
+
+export const InstanceActions = ({ actionGroups }: IInstanceActionsProps) => (
+  <div style={{ display: 'inline-block' }}>
+    <Dropdown className="dropdown" id="instace-actions-dropdown">
+      <Dropdown.Toggle className="btn btn-sm btn-primary dropdown-toggle">
+        <span>Instance Actions</span>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        {(actionGroups || [])
+          .filter((group) => group.length)
+          .map((group, i) => {
+            return (
+              <>
+                {group.map((action) => (
+                  <li id={`instance-action-${action.label}`}>
+                    <a onClick={action.triggerAction}>{action.label}</a>
+                  </li>
+                ))}
+                {i !== actionGroups.length - 1 && <li role="presentation" className="divider"></li>}
+              </>
+            );
+          })}
+      </Dropdown.Menu>
+    </Dropdown>
+  </div>
+);
