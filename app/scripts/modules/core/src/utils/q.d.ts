45236b38cfb7c19c9478f7298ccc9e973be890bf
Chris Thielen <github@sandgnat.com>
2020-10-20 17:43:17 -0700

    refactor(core): Introduce  -> PromiseLike kludge, start refactoring code to use PromiseLike

--- app/scripts/modules/core/src/utils/q.d.ts
+++ app/scripts/modules/core/src/utils/q.d.ts
@@ -7,3 +7,18 @@ declare module '@types/angular' {
     resolve<T>(promise: Promise<T>): IPromise<T>;
   }
 }
+
+
+declare global {
+  // This allows an angular $q promise to be returned as a PromiseLike<T>
+  interface PromiseLike<T> {
+    /**
+     * Attaches a callback for only the rejection of the Promise.
+     * @param onrejected The callback to execute when the Promise is rejected.
+     * @returns A Promise for the completion of the callback.
+     */
+    catch<TResult = never>(
+      onrejected?: ((reason: any) => TResult | PromiseLike<TResult>) | undefined | null,
+    ): PromiseLike<T | TResult>;
+  }
+}
