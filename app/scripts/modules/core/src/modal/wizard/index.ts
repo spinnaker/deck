b661caabf4d544f719ef24050681a6194fda41c1
chebebrand <chebebrand@netflix.com>
2020-11-17 16:41:28 -0800

    feat(core/modal): Introduce MultiStepModalWizard

--- app/scripts/modules/core/src/modal/wizard/index.ts
+++ app/scripts/modules/core/src/modal/wizard/index.ts
@@ -1,4 +1,6 @@
 export * from './ModalWizard';
+export * from './MultiStepModalWizard';
 export * from './WizardModal';
 export * from './WizardPage';
+export * from './WizardStep';
 export * from './WizardStepLabel';
