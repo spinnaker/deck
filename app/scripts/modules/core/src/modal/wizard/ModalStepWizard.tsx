e1b36acef15ce9e89fed69abf9980bf89a546ed9
chebebrand <chebebrand@netflix.com>
2020-11-25 12:50:28 -0800

    Refactor so StepWizard only injects props related to step

--- app/scripts/modules/core/src/modal/wizard/ModalStepWizard.tsx
+++ app/scripts/modules/core/src/modal/wizard/ModalStepWizard.tsx
@@ -1,76 +1,131 @@
 import * as React from 'react';
 
-import {
-  IModalComponentProps,
-  IStepWizardInjectedProps,
-  ModalBody,
-  ModalFooter,
-  ModalHeader,
-  StepAction,
-  StepWizard,
-} from 'core/presentation';
+import { IModalComponentProps, ModalBody, ModalFooter, ModalHeader, StepWizard } from 'core/presentation';
 import { Button } from 'core/managed/Button';
 import { NgReact } from 'core/reactShims';
 import { TaskMonitor } from 'core/task';
 
+export interface IStepAction {
+  goToNext?: (currentIndex: number) => number;
+  goToPrevious?: (currentIndex: number) => number;
+  primaryAction?: () => void;
+  primaryActionLabel?: string;
+  secondaryActionType: 'Back' | 'Cancel' | 'None';
+}
+
+export interface IModalActionProps {
+  action: IStepAction;
+  currentIndex: number;
+  dismissModal: () => void;
+  goToStep: (i: number) => void;
+  isValid?: boolean;
+  isLastStep: boolean;
+}
+
 export interface IModalStepWizardProps extends IModalComponentProps {
+  actions: IStepAction[];
   heading: string;
-  taskMonitor?: TaskMonitor;
+  isValid?: boolean;
   steps: React.ReactNode[];
-  stepActions: StepAction[];
-  isValid: boolean;
+  taskMonitor?: TaskMonitor;
 }
 
-export interface IModalStepContentProps extends IStepWizardInjectedProps {
-  isValid: boolean;
-}
-
-export const ModalStepContent = ({
-  currentStep,
-  onPrimaryClick,
-  onSecondaryClick,
-  primaryActionLabel,
-  secondaryActionType,
-  isSubmitting,
+export const ModalAction = ({
+  action,
+  currentIndex,
+  dismissModal,
+  goToStep,
+  isLastStep,
   isValid,
-}: IModalStepContentProps) => (
-  <>
-    <ModalBody>{currentStep}</ModalBody>
+}: IModalActionProps) => {
+  const { goToNext, goToPrevious, primaryAction, primaryActionLabel, secondaryActionType } = action;
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+
+  const goToNextStep = () => {
+    if (goToNext) {
+      const newStep = goToNext(currentIndex);
+      goToStep(newStep);
+    } else {
+      if (isLastStep) {
+        dismissModal();
+      } else {
+        goToStep(currentIndex + 1);
+      }
+    }
+  };
+
+  const onPrimaryClick = () => {
+    if (primaryAction) {
+      setIsSubmitting(true);
+      Promise.resolve(primaryAction()).then(() => {
+        setIsSubmitting(false);
+        goToNextStep();
+      });
+    } else {
+      goToNextStep();
+    }
+  };
+
+  const onSecondaryClick = () => {
+    if (secondaryActionType === 'Cancel' || currentIndex === 0) {
+      dismissModal();
+    }
+
+    if (secondaryActionType === 'Back' && goToPrevious) {
+      const newStep = goToPrevious(currentIndex);
+      goToStep(newStep);
+    }
+
+    if (secondaryActionType == 'Back' && currentIndex !== 0) {
+      goToStep(currentIndex - 1);
+    }
+  };
+
+  return (
     <ModalFooter
       primaryActions={
         <div className="flex-container-h sp-group-margin-s-xaxis">
           {secondaryActionType !== 'None' && <Button onClick={onSecondaryClick}>{secondaryActionType}</Button>}
-          <Button appearance="primary" disabled={!isValid || isSubmitting} onClick={onPrimaryClick}>
+          <Button appearance="primary" disabled={isValid === false || isSubmitting} onClick={onPrimaryClick}>
             {!isSubmitting && (primaryActionLabel || 'Next')}
             {isSubmitting && <i className="fa fa-spinner fa-spin" />}
           </Button>
         </div>
       }
     />
-  </>
-);
+  );
+};
 
 export const ModalStepWizard = ({
+  actions,
   heading,
   dismissModal,
-  steps,
-  stepActions,
-  taskMonitor,
   isValid,
+  steps,
+  taskMonitor,
 }: IModalStepWizardProps) => {
-  const wizardProps = {
-    endWizard: dismissModal,
-    steps,
-    stepActions,
-  };
-  const WrappedModalStepContent = StepWizard(ModalStepContent, wizardProps);
   const { TaskMonitorWrapper } = NgReact;
 
   return (
     <React.Fragment>
       <TaskMonitorWrapper monitor={taskMonitor} />
       <ModalHeader>{heading}</ModalHeader>
-      <WrappedModalStepContent isValid={isValid} />
+      <StepWizard
+        steps={steps}
+        render={({ currentIndex, currentStep, goToStep }) => (
+          <React.Fragment>
+            <ModalBody>{currentStep}</ModalBody>
+            <ModalAction
+              action={actions[currentIndex]}
+              currentIndex={currentIndex}
+              dismissModal={dismissModal}
+              goToStep={goToStep}
+              isLastStep={currentIndex === steps.length - 1}
+              isValid={isValid}
+            />
+          </React.Fragment>
+        )}
+      />
     </React.Fragment>
   );
 };
--- app/scripts/modules/core/src/presentation/stepWizard/StepWizard.tsx
+++ app/scripts/modules/core/src/presentation/stepWizard/StepWizard.tsx
@@ -1,88 +1,20 @@
 import * as React from 'react';
 
-export interface StepAction {
-  goToNext?: (currentIndex: number) => number;
-  goToPrevious?: (currentIndex: number) => number;
-  primaryAction?: () => void;
-  primaryActionLabel?: string;
-  secondaryActionType: 'Back' | 'Cancel' | 'None';
-}
-
 export interface IStepWizardInjectedProps {
+  currentIndex: number;
   currentStep: React.ReactNode;
-  onPrimaryClick: () => void;
-  onSecondaryClick: () => void;
-  primaryActionLabel?: string;
-  secondaryActionType: 'Back' | 'Cancel' | 'None';
-  isSubmitting: boolean;
+  goToStep: (i: number) => void;
 }
 
 export interface IStepWizardProps {
-  endWizard: () => void;
   steps: React.ReactNode[];
-  stepActions: StepAction[];
+  render: (props: IStepWizardInjectedProps) => React.ReactNode;
 }
 
-export const StepWizard = <T extends {}>(
-  BaseComponent: React.ComponentType<T>,
-  { endWizard, steps, stepActions }: IStepWizardProps,
-): React.ComponentType<Omit<T, keyof IStepWizardInjectedProps>> => {
+export const StepWizard = ({ render, steps }: IStepWizardProps) => {
   const [currentIndex, setCurrentIndex] = React.useState(0);
-  const [isSubmitting, setIsSubmitting] = React.useState(false);
-
   const currentStep = steps[currentIndex];
-  const currentActions = stepActions[currentIndex];
-  const { goToNext, primaryAction, primaryActionLabel, secondaryActionType, goToPrevious } = currentActions;
+  const goToStep = (i: number) => setCurrentIndex(i);
 
-  const goToNextStep = () => {
-    if (goToNext) {
-      const newStep = goToNext(currentIndex);
-      setCurrentIndex(newStep);
-    } else {
-      if (currentIndex === steps.length - 1) {
-        endWizard();
-      } else {
-        setCurrentIndex(currentIndex + 1);
-      }
-    }
-  };
-
-  const onPrimaryClick = () => {
-    if (primaryAction) {
-      setIsSubmitting(true);
-      Promise.resolve(primaryAction()).then(() => {
-        setIsSubmitting(false);
-        goToNextStep();
-      });
-    } else {
-      goToNextStep();
-    }
-  };
-
-  const onSecondaryClick = () => {
-    if (secondaryActionType === 'Cancel' || currentIndex === 0) {
-      endWizard();
-    }
-
-    if (secondaryActionType === 'Back' && goToPrevious) {
-      const newStep = goToPrevious(currentIndex);
-      setCurrentIndex(newStep);
-    }
-
-    setCurrentIndex(currentIndex - 1);
-  };
-
-  const WrappedComponent = (props: T) => (
-    <BaseComponent
-      {...props}
-      currentStep={currentStep}
-      onPrimaryClick={onPrimaryClick}
-      onSecondayClick={onSecondaryClick}
-      primaryActionLabel={primaryActionLabel}
-      secondaryActionType={secondaryActionType}
-      isSubmitting={isSubmitting}
-    />
-  );
-
-  return WrappedComponent;
+  return <React.Fragment>{render({ currentIndex, currentStep, goToStep })}</React.Fragment>;
 };
