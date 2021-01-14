12cdbed1b0444db1a8ed57e9912576256cb45cde
chebebrand <chebebrand@netflix.com>
2021-01-13 16:21:06 -0800

    fix(core/executions): Protect migrationStatus sorting if config is null

--- app/scripts/modules/core/src/pipeline/executions/executionGroup/ExecutionGroups.tsx
+++ app/scripts/modules/core/src/pipeline/executions/executionGroup/ExecutionGroups.tsx
@@ -90,7 +90,7 @@ export class ExecutionGroups extends React.Component<IExecutionGroupsProps, IExe
     const hasGroups = groups.length > 0;
     const className = `row pipelines executions ${showingDetails ? 'showing-details' : ''}`;
 
-    const allGroups = groups
+    const allGroups = (groups || [])
       .filter((g: IExecutionGroup) => g?.config?.migrationStatus === 'Started')
       .concat(groups.filter((g) => g?.config?.migrationStatus !== 'Started'));
 
