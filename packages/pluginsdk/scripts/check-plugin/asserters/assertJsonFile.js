5718f379241ffaaa11e8c541bf54b9a48440e4be
Chris Thielen <github@sandgnat.com>
2021-02-12 13:30:05 -1000

    feat(pluginsdk): Add support for "field should not exist" in check-plugin lint - Add support for deleting a field in a json file

--- packages/pluginsdk/scripts/check-plugin/asserters/assertJsonFile.js
+++ packages/pluginsdk/scripts/check-plugin/asserters/assertJsonFile.js
@@ -1,15 +1,25 @@
-const { get } = require('lodash');
+const { get, has } = require('lodash');
 
 const assertJsonFile = (report, filename, json) => {
   return function assertJsonFile(field, expectedValue) {
-    const currentValue = get(json, field);
-    const resolution = {
-      description: `Change ${field} in ${filename} from "${currentValue}" to "${expectedValue}"`,
-      command: `npx write-json "${filename}" "${field}" "${expectedValue}"`,
-    };
-    const ok = currentValue === expectedValue;
-    report(`Unexpected value in ${filename}: ${field} should be "${expectedValue}"`, ok, resolution);
+    if (expectedValue === DELETED_FIELD) {
+      const ok = !has(json, field);
+      const resolution = {
+        description: `Delete ${field} in ${filename}`,
+        command: `npx write-json --delete "${filename}" "${field}"`,
+      };
+      report(`Unexpected property in ${filename}: ${field}`, ok, resolution);
+    } else {
+      const currentValue = get(json, field);
+      const resolution = {
+        description: `Change ${field} in ${filename} from "${currentValue}" to "${expectedValue}"`,
+        command: `npx write-json "${filename}" "${field}" "${expectedValue}"`,
+      };
+      const ok = currentValue === expectedValue;
+      report(`Unexpected value in ${filename}: ${field} should be "${expectedValue}"`, ok, resolution);
+    }
   };
 };
 
-module.exports = { assertJsonFile };
+const DELETED_FIELD = {};
+module.exports = { assertJsonFile, DELETED_FIELD };
