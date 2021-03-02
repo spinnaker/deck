9c209f6db838078c8fdf0f2717251295c57efb58
Jorge <jorgebee65@hotmail.com>
2021-03-02 16:06:31 -0600

    feat(blockDevice/tags): add other volumeTypes

--- app/scripts/modules/amazon/src/serverGroup/configure/serverGroupConfiguration.service.ts
+++ app/scripts/modules/amazon/src/serverGroup/configure/serverGroupConfiguration.service.ts
@@ -133,7 +133,7 @@ export class AwsServerGroupConfigurationService {
     'GroupTotalInstances',
   ];
   private healthCheckTypes = ['EC2', 'ELB'];
-  private volumeTypes = ['gp2'];
+  private volumeTypes = ['gp2', 'st1', 'io1', 'sc1'];
   private terminationPolicies = [
     'OldestInstance',
     'NewestInstance',
