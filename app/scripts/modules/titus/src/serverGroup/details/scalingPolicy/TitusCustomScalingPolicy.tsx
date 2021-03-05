e5d48dfa82868c1464a6d32924897a21f889011a
chebebrand <chebebrand@netflix.com>
2021-03-05 11:30:29 -0800

    feat(titus): Use overridableComponent

--- app/scripts/modules/titus/src/serverGroup/details/scalingPolicy/TitusCustomScalingPolicy.tsx
+++ app/scripts/modules/titus/src/serverGroup/details/scalingPolicy/TitusCustomScalingPolicy.tsx
@@ -1,14 +1,12 @@
 import * as React from 'react';
-import { Application, IServerGroup, Overridable } from '@spinnaker/core';
+import { Application, IServerGroup, overridableComponent } from '@spinnaker/core';
 
 export interface ITitusCustomScalingPolicyProps {
   application: Application;
   serverGroup: IServerGroup;
 }
 
-@Overridable('titus.serverGroup.details.customScaling')
-export class TitusCustomScalingPolicy extends React.Component<ITitusCustomScalingPolicyProps> {
-  public render(): any {
-    return null;
-  }
-}
+export const TitusCustomScalingPolicy: React.ComponentType<ITitusCustomScalingPolicyProps> = overridableComponent(
+  () => null,
+  'titus.serverGroup.details.customScaling',
+);
