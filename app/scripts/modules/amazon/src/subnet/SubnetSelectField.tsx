1d700d1bf6d2be391541ef8f7a0608ebfaf457e8
Chris Thielen <christopherthielen@users.noreply.github.com>
2021-03-05 08:50:30 -1000

    fix(amazon/subnet): fix NPE when AWSProviderSettings.serverGroups is undefined (#8976)

--- app/scripts/modules/amazon/src/subnet/SubnetSelectField.tsx
+++ app/scripts/modules/amazon/src/subnet/SubnetSelectField.tsx
@@ -28,8 +28,11 @@ export class SubnetSelectField extends React.Component<ISubnetSelectFieldProps> 
   public render() {
     const { labelColumns, helpKey, component, region, field, ...otherProps } = this.props;
     const value = component[field];
-    const isRecommended = (AWSProviderSettings.serverGroups?.recommendedSubnets || []).includes(value);
-    const { subnetWarning } = AWSProviderSettings.serverGroups;
+    const isRecommended = some(
+      AWSProviderSettings.serverGroups?.recommendedSubnets || [],
+      (subnet) => value && value.includes(subnet),
+    );
+    const subnetWarning = AWSProviderSettings.serverGroups?.subnetWarning;
     return (
       <div className="form-group">
         <div className={`col-md-${labelColumns} sm-label-right`}>
