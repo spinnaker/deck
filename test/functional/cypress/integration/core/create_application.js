2ec75982720c26f7e6bca22fbce7763c3c7f9ec9
Barbara Brina <barbara.brina@armory.io>
2021-03-17 15:58:59 -0600

    fix(core/InsightMenu): Fixes functional test

--- test/functional/cypress/integration/core/create_application.js
+++ test/functional/cypress/integration/core/create_application.js
@@ -4,7 +4,7 @@ describe('core: Create Application', () => {
   beforeEach(() => registerDefaultFixtures());
   it(`shows a config screen with required fields preventing creation until they're populated`, () => {
     cy.visit('#/applications');
-    cy.get('a:contains("Create Application")').click();
+    cy.get('button:contains("Create Application")').click();
 
     cy.get('button:contains("Create")').should('be.disabled');
     cy.get('input[name=name]').type('testapp');
@@ -19,7 +19,7 @@ describe('core: Create Application', () => {
     cy.route('/applications/testapp1?expand=false', 'fixture:core/create_application/testapp1.json');
 
     cy.visit('#/applications');
-    cy.get('a:contains("Create Application")').click();
+    cy.get('button:contains("Create Application")').click();
 
     cy.get('input[name=name]').type('testapp1');
     cy.get('input[name=email]').type('user@testcompany.com');
