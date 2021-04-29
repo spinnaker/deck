69d1adf68362ccfa3fca12dbbec66476e524a96e
Rani <rhorev@netflix.com>
2021-04-29 15:20:42 -0700

    fix(pr): make version metadata pretty again :)

--- app/scripts/modules/core/src/managed/overview/artifact/VersionMetadata.tsx
+++ app/scripts/modules/core/src/managed/overview/artifact/VersionMetadata.tsx
@@ -4,6 +4,10 @@ import React from 'react';
 import { IconTooltip } from 'core/index';
 import { RelativeTimestamp } from '../../RelativeTimestamp';
 
+const MetadataElement: React.FC = ({ children }) => {
+  return <span className="metadata-element">{children}</span>;
+};
+
 export const VersionMetadata = ({
   buildNumber,
   author,
@@ -17,39 +21,24 @@ export const VersionMetadata = ({
   buildDuration?: string;
   commitsBehind?: number;
 }) => {
-  const elements = [
-    <>Build #{buildNumber}</>,
-    author ? <>By {author}</> : undefined,
-    deployedAt ? (
-      <>
-        <div className="metadata-icon">
-          <IconTooltip tooltip="Deployed at" name="cloudDeployed" size="12px" />
-        </div>
-        <RelativeTimestamp timestamp={DateTime.fromISO(deployedAt)} className="" style={{}} /> ago
-      </>
-    ) : undefined,
-    buildDuration ? (
-      <>
-        <div className="metadata-icon">
-          <IconTooltip tooltip="Build duration" name="build" size="12px" />
-        </div>
-        {buildDuration}
-      </>
-    ) : undefined,
-    commitsBehind ? <>{commitsBehind}</> : undefined,
-  ].filter(Boolean);
-
   return (
     <div>
       <div className="version-metadata">
-        {elements.map((elem, index) => {
-          return (
-            <React.Fragment key={index}>
-              {index > 0 && <span className="metatdata-separator">•</span>}
-              {elem}
-            </React.Fragment>
-          );
-        })}
+        <MetadataElement>Build #{buildNumber}</MetadataElement>
+        {author && <MetadataElement>By {author}</MetadataElement>}
+        {deployedAt && (
+          <MetadataElement>
+            <IconTooltip tooltip="Deployed at" name="cloudDeployed" size="12px" wrapperClassName="metadata-icon" />
+            <RelativeTimestamp timestamp={DateTime.fromISO(deployedAt)} className="" style={{}} /> ago
+          </MetadataElement>
+        )}
+        {buildDuration && (
+          <MetadataElement>
+            <IconTooltip tooltip="Build duration" name="build" size="12px" wrapperClassName="metadata-icon" />
+            {buildDuration}
+          </MetadataElement>
+        )}
+        {commitsBehind && <MetadataElement>{commitsBehind}</MetadataElement>}
       </div>
     </div>
   );
