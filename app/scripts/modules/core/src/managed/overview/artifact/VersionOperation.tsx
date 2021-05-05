8fd9f117b3cdec868f09867ddd2c8b20c373f39f
Rani <rhorev@netflix.com>
2021-05-05 13:40:33 -0700

    feat(md): resource actuation state + bug fixes

--- app/scripts/modules/core/src/managed/overview/artifact/VersionOperation.tsx
+++ app/scripts/modules/core/src/managed/overview/artifact/VersionOperation.tsx
@@ -2,16 +2,19 @@ import React from 'react';
 
 import { QueryConstraint, QueryVerificationStatus } from '../types';
 
+type AllStatuses = QueryConstraint['status'] | QueryVerificationStatus;
+
 const constraintStatusUtils: {
-  [key in QueryConstraint['status'] | QueryVerificationStatus]: { color?: string; icon: string };
+  [key in AllStatuses]: { color?: string; icon: string };
 } = {
   FAIL: { color: 'var(--color-status-error)', icon: 'fas fa-times' },
   FORCE_PASS: { color: 'var(--color-status-success)', icon: 'fas fa-check' },
   PASS: { color: 'var(--color-status-success)', icon: 'fas fa-check' },
   PENDING: { icon: 'far fa-hourglass' },
+  NOT_EVALUATED: { icon: 'far fa-hourglass' },
 };
 
-export const VersionOperationIcon = ({ status }: { status: QueryConstraint['status'] }) => {
+export const VersionOperationIcon = ({ status }: { status: AllStatuses }) => {
   return (
     <i
       className={constraintStatusUtils[status].icon}
