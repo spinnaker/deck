adb69d835d438a3c3aa53b9b7b421503f05da05b
chebebrand <chebebrand@netflix.com>
2021-05-07 15:04:34 -0700

    fix(core/pipeline): Warn if concurrency disabled on stage restart
    fix(core/pipeline): Warn if concurrency disabled on stage restart

--- app/scripts/modules/core/src/pipeline/config/stages/stage.module.js
+++ app/scripts/modules/core/src/pipeline/config/stages/stage.module.js
@@ -357,6 +357,12 @@ module(CORE_PIPELINE_CONFIG_STAGES_STAGE_MODULE, [
           body =
             '<p><strong>This pipeline is currently running - restarting this stage will result in multiple concurrently running pipelines.</strong></p>';
         }
+
+        if ($scope.execution.isRunning && $scope.execution.limitConcurrent) {
+          body =
+            '<p class="alert alert-warning"><i class="fa fa-exclamation-triangle sp-margin-xs-right"></i>This stage <strong>will not</strong> restart until the running execution completes since concurrency is disabled for this pipeline';
+        }
+
         ConfirmationModalService.confirm({
           header: 'Really restart ' + $scope.stage.name + '?',
           buttonText: 'Restart ' + $scope.stage.name,
