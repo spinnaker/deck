d7cc5d5fb1f3282d76ea3d95dda7d4c1b906d679
chebebrand <chebebrand@netflix.com>
2021-05-12 10:13:34 -0700

    refactor(serverGroup): useCallback with popover content

--- app/scripts/modules/core/src/serverGroup/pod/RunningTasksTag.tsx
+++ app/scripts/modules/core/src/serverGroup/pod/RunningTasksTag.tsx
@@ -12,13 +12,18 @@ export interface IRunningTasksTagProps {
 export const RunningTasksTag = ({ executions, tasks }: IRunningTasksTagProps) => {
   const runningExecutions = (executions || []).filter((e) => e.isRunning || e.hasNotStarted);
 
+  const PopoverContent = React.useCallback(
+    () => <RunningTasksPopoverContent executions={runningExecutions} tasks={tasks} />,
+    [tasks, runningExecutions],
+  );
+
   if (!tasks?.length && !runningExecutions.length) {
     return null;
   }
 
   return (
     <span className="RunningTasksTag">
-      <HoverablePopover Component={() => <RunningTasksPopoverContent executions={runningExecutions} tasks={tasks} />}>
+      <HoverablePopover Component={PopoverContent}>
         <span className="icon">
           <span className="glyphicon icon-spinner fa-spin-slow"></span>
         </span>
