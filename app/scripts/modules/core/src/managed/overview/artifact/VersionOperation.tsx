b54efdc5542bc1e35ff93cc16c2a320c3760c6fc
Rani <rhorev@netflix.com>
2021-05-13 05:58:07 -0700

    feat(md): handle the new BLOCKED state (mainly in manual judgement as it only relevant for stateful constraints)

--- app/scripts/modules/core/src/managed/overview/artifact/VersionOperation.tsx
+++ app/scripts/modules/core/src/managed/overview/artifact/VersionOperation.tsx
@@ -3,16 +3,27 @@ import React from 'react';
 import { QueryConstraint, QueryVerificationStatus } from '../types';
 
 type AllStatuses = QueryConstraint['status'] | QueryVerificationStatus;
+export const ACTION_DISPLAY_NAMES = ['passed', 'overridden', 'pending', 'failed'] as const;
+export type ActionDisplayName = typeof ACTION_DISPLAY_NAMES[number];
 
-export const actionStatusUtils: {
-  [key in AllStatuses]: { color?: string; icon: string; displayName: string };
-} = {
+const DEFAULT_ICON = 'far fa-hourglass';
+
+type ActionStatusUtils = {
+  [key in AllStatuses]: { color?: string; icon: string; displayName: ActionDisplayName };
+};
+
+// This ensures that we cover all of the options
+const actionStatusUtilsInternal: ActionStatusUtils = {
   FAIL: { color: 'var(--color-status-error)', icon: 'fas fa-times', displayName: 'failed' },
   FORCE_PASS: { color: 'var(--color-status-success)', icon: 'fas fa-check', displayName: 'overridden' },
   PASS: { color: 'var(--color-status-success)', icon: 'fas fa-check', displayName: 'passed' },
-  PENDING: { icon: 'far fa-hourglass', displayName: 'pending' },
-  NOT_EVALUATED: { icon: 'far fa-hourglass', displayName: 'pending' },
+  PENDING: { icon: DEFAULT_ICON, displayName: 'pending' },
+  NOT_EVALUATED: { icon: DEFAULT_ICON, displayName: 'pending' },
+  BLOCKED: { icon: DEFAULT_ICON, displayName: 'pending' },
 };
+
+// This forces the consumer to verify that the value exists (important in the case of a breaking change on the backend)
+export const actionStatusUtils: Partial<ActionStatusUtils> = actionStatusUtilsInternal;
 
 interface IVersionOperationIconProps {
   status: AllStatuses;
@@ -21,8 +32,8 @@ interface IVersionOperationIconProps {
 export const VersionOperationIcon = ({ status }: IVersionOperationIconProps) => {
   return (
     <i
-      className={actionStatusUtils[status].icon}
-      style={{ color: actionStatusUtils[status].color || 'var(--color-titanium)' }}
+      className={actionStatusUtils[status]?.icon || DEFAULT_ICON}
+      style={{ color: actionStatusUtils[status]?.color || 'var(--color-titanium)' }}
     />
   );
 };
