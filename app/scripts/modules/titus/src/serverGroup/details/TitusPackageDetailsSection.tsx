ec24e74460b0bdbe753db197a3f6128744624c7a
chebebrand <chebebrand@netflix.com>
2021-05-18 13:33:40 -0700

    feat(titus/serverGroup): Add package details section

---
+++ app/scripts/modules/titus/src/serverGroup/details/TitusPackageDetailsSection.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { CollapsibleSection, LabeledValue, LabeledValueList } from '@spinnaker/core';
+import { DockerImageUtils } from '@spinnaker/docker';
+import { ITitusBuildInfo } from 'titus/domain';
+
+export interface ITitusPackageDetailsSectionProps {
+  buildInfo: ITitusBuildInfo;
+}
+
+export const TitusPackageDetailsSection = ({ buildInfo }: ITitusPackageDetailsSectionProps) => {
+  const packageInfo = buildInfo.jenkins || {};
+  const hasPackageInfo = Boolean(Object.keys(packageInfo).length);
+
+  const { commitId, host, name, number, version } = packageInfo;
+  const { repository } = DockerImageUtils.splitImageId(buildInfo.docker?.image);
+  const jenkinsLink = `${host}job/${name}/${number}`;
+
+  return (
+    <CollapsibleSection heading="Package">
+      {!hasPackageInfo && <div>No package information available.</div>}
+      {hasPackageInfo && (
+        <LabeledValueList className="horizontal-when-filters-collapsed">
+          {name && <LabeledValue label="Job" value={name} />}
+          {repository && <LabeledValue label="Package" value={repository} />}
+          {number && <LabeledValue label="Build" value={number} />}
+          {commitId && <LabeledValue label="Commit" value={commitId.substring(0, 8)} />}
+          {version && <LabeledValue label="Version" value={version} />}
+          {host && (
+            <LabeledValue
+              label="Build Link"
+              value={
+                <a target="_blank" href={jenkinsLink}>
+                  {jenkinsLink}
+                </a>
+              }
+            />
+          )}
+        </LabeledValueList>
+      )}
+    </CollapsibleSection>
+  );
+};
