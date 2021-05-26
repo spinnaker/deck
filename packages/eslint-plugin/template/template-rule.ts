af57bcd52197bc56c7ba3dd95c97f1adff96f06e
Chris Thielen <github@sandgnat.com>
2021-05-26 09:42:17 -1000

    refactor(eslint-plugin): create template rule

---
+++ packages/eslint-plugin/template/template-rule.ts
@@ -0,0 +1,22 @@
+import { Rule } from 'eslint';
+
+const ruleName = 'RULENAME';
+const rule: Rule.RuleModule = {
+  create: (context) => ({
+    ImportSpecifier(node) {
+      if (node.local.name === 'API') {
+        const message = 'Do not import API';
+        context.report({ node, message, fix: (fixer) => fixer.remove(node) });
+      }
+    },
+  }),
+  meta: {
+    fixable: 'code',
+    type: 'problem',
+    docs: {
+      description: 'Do not import API',
+    },
+  },
+};
+
+export { rule, ruleName };
