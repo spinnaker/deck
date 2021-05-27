702e24ba6eb138bc038ff16348ef4b61476b02f7
Chris Thielen <github@sandgnat.com>
2021-05-27 08:44:05 -1000

    refactor(eslint-plugin): migrate utils to typescript

--- packages/eslint-plugin/rules/import-relative-within-subpackage.spec.js
+++ packages/eslint-plugin/rules/import-relative-within-subpackage.spec.js
@@ -1,8 +1,4 @@
-import mockModule from '../utils/mockModule';
-const mock = mockModule('../utils/import-aliases');
-mock.getAllSpinnakerPackages.mockImplementation(() => ['core', 'amazon', 'kubernetes']);
-
-import path from 'path';
+import '../utils/import-aliases.mock';
 import ruleTester from '../utils/ruleTester';
 import rule from '../rules/import-relative-within-subpackage';
 
