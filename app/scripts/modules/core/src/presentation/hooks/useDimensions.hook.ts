ede4bfe3742c01a3b18c4d51f27c3f03b27f95e7
Rani <rhorev@netflix.com>
2021-06-03 20:06:44 -0700

    fix(pr): removed unused hook

--- app/scripts/modules/core/src/presentation/hooks/useDimensions.hook.ts
+++ app/scripts/modules/core/src/presentation/hooks/useDimensions.hook.ts
@@ -1,30 +1,6 @@
 import { debounce } from 'lodash';
 import React from 'react';
 
-export const useWindowDimensions = ({ delay = 200, isActive = true }: { delay?: number; isActive?: boolean }) => {
-  const [dimension, setDimension] = React.useState({ width: window.innerWidth, height: window.innerHeight });
-
-  React.useLayoutEffect(() => {
-    const debouncedResizeHandler = debounce(() => {
-      setDimension({ width: window.innerWidth, height: window.innerHeight });
-    }, delay);
-    if (isActive) {
-      window.addEventListener('resize', debouncedResizeHandler);
-      return () => window.removeEventListener('resize', debouncedResizeHandler);
-    } else {
-      return () => {};
-    }
-  }, [delay, isActive]);
-
-  React.useLayoutEffect(() => {
-    if (isActive) {
-      setDimension({ width: window.innerWidth, height: window.innerHeight });
-    }
-  }, [isActive]);
-
-  return dimension;
-};
-
 const getElementDimensions = (ref: React.RefObject<HTMLElement>) =>
   ref.current ? { width: ref.current.offsetWidth, height: ref.current.offsetHeight } : { width: 0, height: 0 };
 
