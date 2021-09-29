8366e0aef8b9bf8a6a29bd4caeab7cee0d0288c9
Rani <rhorev@netflix.com>
2021-09-28 18:32:08 -0700

    feat(md): added a default component if preview environments configured but don't exist

---
+++ packages/core/src/managed/overview/PreviewEnvironments.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { EnvironmentOverview } from './EnvironmentOverview';
+import { EnvironmentsRender, OrderedEnvironments } from '../environmentBaseElements/EnvironmentsRender';
+import { QueryEnvironment } from './types';
+import { getDocsUrl } from '../utils/defaults';
+import { useLogEvent } from '../utils/logging';
+
+interface IPreviewEnvironmentsProps {
+  orderedEnvironments: OrderedEnvironments<QueryEnvironment>;
+  isConfigured?: boolean;
+}
+
+export const PreviewEnvironments = ({ orderedEnvironments, isConfigured }: IPreviewEnvironmentsProps) => {
+  const { environments: previewEnvironments, ...previewEnvironmentsProps } = orderedEnvironments;
+  const logClick = useLogEvent('PreviewEnvironments');
+  const docsLink = getDocsUrl('previewEnvironments');
+  return (
+    <>
+      {(Boolean(previewEnvironments.length) || isConfigured) && (
+        <h4 className="sp-margin-2xl-top sp-margin-m-bottom self-left">
+          <b>Preview Environments</b>
+        </h4>
+      )}
+      {!Boolean(previewEnvironments.length) && isConfigured && (
+        <div className="self-left">
+          No PRs matching your branch filter were found.
+          <br />
+          Your preview environments will automatically appear here once you create a PR from a branch matching your
+          config definition.{' '}
+          {docsLink && (
+            <a target="_blank" onClick={() => logClick({ action: 'Learn More' })} href={docsLink}>
+              Learn More
+            </a>
+          )}
+        </div>
+      )}
+      <EnvironmentsRender {...previewEnvironmentsProps}>
+        {previewEnvironments.map((env) => (
+          <EnvironmentOverview key={env.name} environment={env} />
+        ))}
+      </EnvironmentsRender>
+    </>
+  );
+};
