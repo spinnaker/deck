7c1a1e3015c51690d995797fbad6192a1476522d
Chris Thielen <github@sandgnat.com>
2021-10-12 10:50:33 -1000

    enable spot pricing

--- packages/app/src/settings-local.js
+++ packages/app/src/settings-local.js
@@ -3,3 +3,18 @@
   e.g.,
   window.spinnakerSettings.defaultInstancePort = 8080;
 */
+function set(obj, path, val) {
+  const segments = path.split('.');
+  const lastSegment = segments.pop();
+  var current = obj;
+  segments.forEach((s) => {
+    current[s] = current[s] || {};
+    current = current[s];
+  });
+  current[lastSegment] = val;
+}
+
+set(window.spinnakerSettings, 'providers.aws.serverGroups.enableLaunchTemplates', true);
+set(window.spinnakerSettings, 'providers.aws.serverGroups.enableIPv6', true);
+set(window.spinnakerSettings, 'providers.aws.serverGroups.enableIMDSv2', true);
+set(window.spinnakerSettings, 'providers.aws.serverGroups.enableCpuCredits', true);
