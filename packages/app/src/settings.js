3b339c034da842cdba9c618357bbfba22db198d8
Ovidiu Popa <ovidiu.popa@armory.io>
2023-11-22 20:21:19 +0200

    fix: Fix conflicts

--- packages/app/src/settings.js
+++ packages/app/src/settings.js
@@ -50,15 +50,11 @@ const managedServiceAccountsEnabled =
   process.env.MANAGED_SERVICE_ACCOUNTS_ENABLED === 'true';
 const managedResourcesEnabled =
   import.meta.env.VITE_MANAGED_RESOURCES_ENABLED === 'true' || process.env.MANAGED_RESOURCES_ENABLED === 'true';
-<<<<<<< HEAD
-const manualJudgmentParentPipelineEnabled = import.meta.env.MJ_PARENTPIPELINE_ENABLED === 'true' || false;
-=======
 const manualJudgmentParentPipelineEnabled = import.meta.env.MJ_PARENTPIPELINE_ENABLED !== 'false';
 const multiBlockFailureMessagesEnabled =
   import.meta.env.MULTI_BLOCK_FAILURE_MESSAGES_ENABLED === 'true' ||
   process.env.MULTI_BLOCK_FAILURE_MESSAGES_ENABLED === 'true' ||
   false;
->>>>>>> 374f724de2 (feat: Add feature flag for multi block failure messages. (#10061))
 const onDemandClusterThreshold =
   import.meta.env.VITE_ON_DEMAND_CLUSTER_THRESHOLD || process.env.ON_DEMAND_CLUSTER_THRESHOLD || '350';
 const reduxLoggerEnabled = import.meta.env.VITE_REDUX_LOGGER === 'true' || process.env.REDUX_LOGGER === 'true';
